223cd7ee771f2d9eb72e04438bfc378861d06bf8
Aleksandr Mikhalev <muxasizhevsk@gmail.com>
2023-02-07 11:04:41 +0300

    Improve norm layer and test

--- wrappers/python/nntile/layer/norm.py
+++ wrappers/python/nntile/layer/norm.py
@@ -9,7 +9,7 @@
 #
 # @version 1.0.0
 # @author Aleksandr Mikhalev
-# @date 2023-02-06
+# @date 2023-02-07
 
 import nntile.nntile_core.tensor as tensor
 import numpy as np
@@ -37,8 +37,11 @@ class Norm_fp32:
         self.axis = axis
         self.l = self.x.shape[axis]
         self.eps = eps
+
     @staticmethod
     def generate_block_cyclic(x, dx, axis, eps, next_tag):
+        if x.shape != dx.shape or x.basetile_shape != dx.basetile_shape:
+            raise ValueError
         x_traits = tensor.TensorTraits(x.shape, x.basetile_shape)
         y = tensor.Tensor_fp32(x_traits, x.distribution, next_tag)
         next_tag = y.next_tag
@@ -69,6 +72,67 @@ class Norm_fp32:
         next_tag = y_last.next_tag
         layer = Norm_fp32(x, dx, y, dy, gb, dgb, sumnorm, y_last, axis, eps)
         return (layer, next_tag)
+
+    def rebatch(self, new_x, new_dx, batch_axis, next_tag):
+        # Shortcuts for new and old shapes and basetiles
+        new_shape = new_x.shape
+        new_basetile =new_x.basetile_shape
+        shape = self.x.shape
+        basetile = self.x.basetile_shape
+        # In case nothing is changed do nothing
+        if new_shape == shape and new_basetile == basetile:
+            return self
+        # Check if new and old shapes differ only in 1 axis
+        check_new = new_shape[:batch_axis] + new_shape[batch_axis+1:]
+        check_old = shape[:batch_axis] + shape[batch_axis+1:]
+        if check_new != check_old:
+            raise ValueError
+        # Check if new and old basetiles differ only in 1 axis
+        check_new = new_basetile[:batch_axis] + new_basetile[batch_axis+1:]
+        check_old = basetile[:batch_axis] + basetile[batch_axis+1:]
+        if check_new != check_old:
+            raise ValueError
+        # Check if new X and dX correspond to each other
+        if new_x.shape != new_dx.shape \
+                or new_x.basetile_shape != new_dx.basetile_shape:
+            raise ValueError
+        # Define new Y and dY with the same shape and basetile as new X
+        new_x_traits = tensor.TensorTraits(new_x.shape, new_x.basetile_shape)
+        new_y = tensor.Tensor_fp32(new_x_traits, new_x.distribution, next_tag)
+        next_tag = new_y.next_tag
+        new_dy = tensor.Tensor_fp32(new_x_traits, new_x.distribution, next_tag)
+        next_tag = new_dy.next_tag
+        # If normalization axis is the same as batch_axis then we can use the
+        # same sumnorm as before
+        if batch_axis == self.axis:
+            new_sumnorm = self.sumnorm
+        else:
+            new_sumnorm_shape = [2] + new_shape[:self.axis] \
+                    + new_shape[self.axis+1:]
+            new_sumnorm_basetile = [2] + new_basetile[:self.axis] \
+                    + new_basetile[self.axis+1:]
+            new_sumnorm_traits = tensor.TensorTraits(new_sumnorm_shape, \
+                    new_sumnorm_basetile)
+            new_sumnorm_distr = []
+            new_x_distr = new_x.distribution
+            for i in range(new_sumnorm_traits.grid.nelems):
+                new_sumnorm_tile_index = \
+                        new_sumnorm_traits.grid.linear_to_index(i)
+                new_x_tile_index = new_sumnorm_tile_index[1:self.axis+1] \
+                        + [0] + new_sumnorm_tile_index[self.axis+1:]
+                new_x_tile_offset = new_x.grid.index_to_linear( \
+                        new_x_tile_index)
+                new_sumnorm_distr.append(new_x_distr[new_x_tile_offset])
+            new_sumnorm = tensor.Tensor_fp32(new_sumnorm_traits, \
+                    new_sumnorm_distr, next_tag)
+            next_tag = new_sumnorm.next_tag
+        new_y_last = tensor.Tensor_fp32(new_x_traits, new_x.distribution, \
+                next_tag)
+        next_tag = new_y_last.next_tag
+        new_layer = Norm_fp32(new_x, new_dx, new_y, new_dy, self.gb, \
+                self.dgb, new_sumnorm, new_y_last, self.axis, self.eps)
+        return (new_layer, next_tag)
+
     def forward_async(self):
         tensor.clear_async_fp32(self.sumnorm)
         tensor.sumnorm_async_fp32(self.x, self.sumnorm, self.axis)
@@ -80,6 +144,7 @@ class Norm_fp32:
         self.gb.wont_use()
         self.sumnorm.wont_use()
         self.y_last.wont_use()
+
     def backward_async(self):
         raise NotImplementedError
 
--- wrappers/python/tests/nntile_core/test_layer_norm.py
+++ wrappers/python/tests/nntile_core/test_layer_norm.py
@@ -9,7 +9,7 @@
 #
 # @version 1.0.0
 # @author Aleksandr Mikhalev
-# @date 2023-02-06
+# @date 2023-02-07
 
 # All necesary imports
 import nntile
@@ -23,13 +23,15 @@ Tensor_fp32 = nntile.tensor.Tensor_fp32
 Norm_fp32 = nntile.layer.Norm_fp32
 
 # Helper function returns bool value true if test passes
-def helper_fp32():
+def helper_main_fp32():
     # Describe single-tile tensor, located at node 0
-    A_shape = [2, 3, 4]
+    A_shape = [4, 5, 6]
+    new_A_shape = [4, 8, 6]
     ndim = len(A_shape)
     dtype = np.float32
     eps = 1e-6
     A_traits = nntile.tensor.TensorTraits(A_shape, A_shape)
+    new_A_traits = nntile.tensor.TensorTraits(new_A_shape, new_A_shape)
     mpi_distr = [0]
     next_tag = 0
     # Tensor objects
@@ -37,39 +39,65 @@ def helper_fp32():
     next_tag = A.next_tag
     dA = Tensor_fp32(A_traits, mpi_distr, next_tag)
     next_tag = dA.next_tag
+    new_A = Tensor_fp32(new_A_traits, mpi_distr, next_tag)
+    next_tag = new_A.next_tag
+    new_dA = Tensor_fp32(new_A_traits, mpi_distr, next_tag)
+    next_tag = new_dA.next_tag
     # Set initial values of tensors
     rand_A = np.random.randn(*A_shape)
     np_A = np.array(rand_A, dtype=dtype, order='F')
+    rand_new_A = np.random.randn(*new_A_shape)
+    np_new_A = np.array(rand_new_A, dtype=dtype, order='F')
     np_B = np.zeros_like(np_A)
+    np_new_B = np.zeros_like(np_new_A)
     # Check result along each axis
     for i in range(ndim):
-        # A is invalidate after each forward_async
+        # A is invalidated after each forward_async
         A.from_array(np_A)
         # Set up normalization layer
-        layer, next_tag = Norm_fp32.generate_block_cyclic(A, dA, i, eps,
+        layer, next_tag = Norm_fp32.generate_block_cyclic(A, dA, i, eps, \
                 next_tag)
+        # Do forward pass and wait until it is finished
         layer.forward_async()
         nntile.starpu.wait_for_all()
+        # Dump output
         layer.y.to_array(np_B)
+        # Check output correctness
         np_A_mean = np_A.mean(axis=i, keepdims=True)
         np_C = np_A - np_A_mean
-        np_A_dev = (np_A.var(axis=i, keepdims=True)+eps)**0.5
+        np_A_dev = (np_A.var(axis=i, keepdims=True)+eps) ** 0.5
         np_C /= np_A_dev
-        if not np.allclose(np_C, np_B, rtol=4e-2, atol=1e-5):
-            import pdb
-            pdb.set_trace()
+        if not np.allclose(np_C, np_B, rtol=1e-4, atol=1e-5):
+            return False
+        # new_A is invalidated after each forward_async
+        new_A.from_array(np_new_A)
+        # Rebatch layer
+        new_layer, next_tag = layer.rebatch(new_A, new_dA, 1, next_tag)
+        # Do forward pass and wait until it is finished
+        new_layer.forward_async()
+        nntile.starpu.wait_for_all()
+        # Dump output
+        new_layer.y.to_array(np_new_B)
+        # Check output correctness
+        np_new_A_mean = np_new_A.mean(axis=i, keepdims=True)
+        np_new_C = np_new_A - np_new_A_mean
+        np_new_A_dev = (np_new_A.var(axis=i, keepdims=True)+eps) ** 0.5
+        np_new_C /= np_new_A_dev
+        if not np.allclose(np_new_C, np_new_B, rtol=1e-4, atol=1e-5):
             return False
     A.unregister()
     dA.unregister()
+    new_A.unregister()
+    new_dA.unregister()
     return True
 
 # Test runner for different precisions
 def test():
-    assert helper_fp32()
+    assert helper_main_fp32()
 
 # Repeat tests
 def test_repeat():
-    assert helper_fp32()
+    assert helper_main_fp32()
 
 if __name__ == "__main__":
     test()
