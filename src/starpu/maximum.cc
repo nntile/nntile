57dd64d41642783977a7a9be74be07d033c61769
Alexandr Katrutsa <aleksandr.katrutsa@phystech.edu>
2023-02-10 23:06:26 +0300

    Exclude cuda from codelet

--- src/starpu/maximum.cc
+++ src/starpu/maximum.cc
@@ -65,7 +65,7 @@ void init()
             nullptr,
             {cpu<fp32_t>},
 #ifdef NNTILE_USE_CUDA
-            {cuda<fp32_t>}
+            // {cuda<fp32_t>}
 #else // NNTILE_USE_CUDA
             {}
 #endif // NNTILE_USE_CUDA
@@ -74,7 +74,7 @@ void init()
             nullptr,
             {cpu<fp64_t>},
 #ifdef NNTILE_USE_CUDA
-            {cuda<fp64_t>}
+            // {cuda<fp64_t>}
 #else // NNTILE_USE_CUDA
             {}
 #endif // NNTILE_USE_CUDA
