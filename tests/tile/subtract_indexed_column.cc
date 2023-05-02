9eea5b288037113df4f5c631920763ca4a2100aa
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2023-05-02 15:19:09 +0300

    Add NotImplemented tests at C++ level

---
+++ tests/tile/subtract_indexed_column.cc
@@ -0,0 +1,23 @@
+/*! @copyright (c) 2022-2023 Skolkovo Institute of Science and Technology
+ *                           (Skoltech). All rights reserved.
+ *
+ * NNTile is software framework for fast training of big neural networks on
+ * distributed-memory heterogeneous systems based on StarPU runtime system.
+ *
+ * @file tests/tile/subtract_indexed_column.cc
+ * Subtract indexed column for Tile<T>
+ *
+ * @version 1.0.0
+ * @author Aleksandr Mikhalev
+ * @date 2023-05-02
+ * */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    // Not implemented
+    std::cout << "This test is not yet implemented\n";
+    return -1;
+}
+
---
+++ wrappers/python/tests/nntile_core/test_tensor_sum_slice.py
@@ -0,0 +1,15 @@
+# @copyright (c) 2022-2023 Skolkovo Institute of Science and Technology
+#                           (Skoltech). All rights reserved.
+#
+# NNTile is software framework for fast training of big neural networks on
+# distributed-memory heterogeneous systems based on StarPU runtime system.
+#
+# @file wrappers/python/tests/nntile_core/test_tensor_sum_slice.py
+# Test for tensor::sum_slice<T> Python wrapper
+#
+# @version 1.0.0
+# @author Aleksandr Mikhalev
+# @date 2023-05-02
+
+# Test is not yet implemented
+raise NotImplementedError
