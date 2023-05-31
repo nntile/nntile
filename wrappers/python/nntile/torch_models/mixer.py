Repair of incorrect normalization layer application. Get rid of useless temporary variables in the nntile mixer layer implementation.

wrappers/python/nntile/torch_models/mixer.py
@@ -33,10 +33,9 @@ class MixerMlp(nn.Module):
 class Mixer(nn.Module):
     def __init__(self, mlp1_w1, mlp1_w2, mlp2_w1, mlp2_w2):       
         super().__init__()
-        dim_1 = mlp1_w1.shape[1]
-        dim_2 = mlp2_w1.shape[0]
-        self.norm_1 = nn.LayerNorm(dim_1)
-        self.norm_2 = nn.LayerNorm(dim_2)
+        norm_dim = mlp2_w1.shape[0]
+        self.norm_1 = nn.LayerNorm(norm_dim)
+        self.norm_2 = nn.LayerNorm(norm_dim)
         self.mlp_1 = MixerMlp('R', mlp1_w1, mlp1_w2)
         self.mlp_2 = MixerMlp('L', mlp2_w1, mlp2_w2)
 
@@ -48,9 +47,7 @@ class Mixer(nn.Module):
         self.norm_2.bias.data = torch.from_numpy(norm_2_beta)
 
 
-    def forward(self, x: torch.Tensor):      
-        xT_norm = self.norm_1(torch.transpose(x, 0, 2)) 
-        x_norm = torch.transpose(xT_norm, 0, 2)   
-        y_tmp = self.mlp_1.forward(x_norm) + x
+    def forward(self, x: torch.Tensor):       
+        y_tmp = self.mlp_1.forward(self.norm_1(x)) + x
         return self.mlp_2.forward(self.norm_2(y_tmp)) + y_tmp
     

wrappers/python/tests/layer/test_mixer_against_torch.py
@@ -73,27 +73,22 @@ def helper(dtype: np.dtype):
     np_W4 = np.array(rand_W4, dtype=dtype, order='F')
     mixer_layer.mlp_2.w2.value.from_array(np_W4)
 
-    rand_gamma = np.random.randn((A_shape[0]))
-    np_gamma1 = np.array(rand_gamma, dtype=dtype, order='F')
-    rand_beta = np.random.randn((A_shape[0]))
-    np_beta1 = np.array(rand_beta, dtype=dtype, order='F')
-
     rand_gamma = np.random.randn((A_shape[2]))
-    np_gamma2 = np.array(rand_gamma, dtype=dtype, order='F')
+    np_gamma = np.array(rand_gamma, dtype=dtype, order='F')
     rand_beta = np.random.randn((A_shape[2]))
-    np_beta2 = np.array(rand_beta, dtype=dtype, order='F')
+    np_beta = np.array(rand_beta, dtype=dtype, order='F')
 
-    mixer_layer.norm_1.gamma.value.from_array(np_gamma1)
-    mixer_layer.norm_1.beta.value.from_array(np_beta1)
+    mixer_layer.norm_1.gamma.value.from_array(np_gamma)
+    mixer_layer.norm_1.beta.value.from_array(np_beta)
 
-    mixer_layer.norm_2.gamma.value.from_array(np_gamma2)
-    mixer_layer.norm_2.beta.value.from_array(np_beta2)
+    mixer_layer.norm_2.gamma.value.from_array(np_gamma)
+    mixer_layer.norm_2.beta.value.from_array(np_beta)
 
     A.from_array(np_A)
     mixer_layer.forward_async()
 
     torch_mixer_layer = torch_Mixer(np_W1, np_W2, np_W3, np_W4)
-    torch_mixer_layer.set_normalization_parameters(np_gamma1, np_beta1, np_gamma2, np_beta2)
+    torch_mixer_layer.set_normalization_parameters(np_gamma, np_beta, np_gamma, np_beta)
     torch_mixer_layer.zero_grad()
     torch_output = torch_mixer_layer.forward(torch.from_numpy(np_A))
     np_Y = np.array(torch_output.detach().numpy(), order="F", dtype=dtype)