253fbc5e3b7ef55edfd2dfeb8ec974f52e883098
Aleksandr Katrutsa <a.katrutsa@skoltech.ru>
2023-06-29 13:34:30 +0300

    Add cuda code for addcdiv

--- tests/kernel/addcdiv.cc
+++ tests/kernel/addcdiv.cc
@@ -24,38 +24,46 @@ using namespace nntile::kernel::addcdiv;
 
 #ifdef NNTILE_USE_CUDA
 template<typename T>
-void run_cuda(Index nelems, const std::vector<T> &src, std::vector<T> &dst)
+void run_cuda(T val, T eps, Index nelems, T* nom, T* denom, T* src)
 {
-    // // Copy to device
-    // T *dev_src, *dev_dst;
-    // cudaError_t cuda_err = cudaMalloc(&dev_src, sizeof(T)*nelems);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // cuda_err = cudaMalloc(&dev_dst, sizeof(T)*nelems);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // cuda_err = cudaMemcpy(dev_src, &src[0], sizeof(T)*nelems,
-    //         cudaMemcpyHostToDevice);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // cuda_err = cudaMemcpy(dev_dst, &dst[0], sizeof(T)*nelems,
-    //         cudaMemcpyHostToDevice);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // // Init stream
-    // cudaStream_t stream;
-    // cuda_err = cudaStreamCreate(&stream);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // // Launch low-level CUDA kernel
-    // cuda<T>(stream, nelems, dev_src, dev_dst);
-    // cuda_err = cudaStreamSynchronize(stream);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // // Copy result and deallocate device memory
-    // cuda_err = cudaMemcpy(&dst[0], dev_dst, sizeof(T)*nelems,
-    //         cudaMemcpyDeviceToHost);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // cuda_err = cudaFree(dev_src);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // cuda_err = cudaFree(dev_dst);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
-    // cuda_err = cudaStreamDestroy(stream);
-    // TEST_ASSERT(cuda_err == cudaSuccess);
+    // Copy to device
+    T *dev_src, *dev_nom, *dev_denom;
+    cudaError_t cuda_err = cudaMalloc(&dev_src, sizeof(T)*nelems);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaMalloc(&dev_nom, sizeof(T)*nelems);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaMalloc(&dev_denom, sizeof(T)*nelems);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+
+    cuda_err = cudaMemcpy(dev_src, &src[0], sizeof(T)*nelems,
+            cudaMemcpyHostToDevice);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaMemcpy(dev_denom, &denom[0], sizeof(T)*nelems,
+            cudaMemcpyHostToDevice);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaMemcpy(dev_nom, &nom[0], sizeof(T)*nelems,
+            cudaMemcpyHostToDevice);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    // Init stream
+    cudaStream_t stream;
+    cuda_err = cudaStreamCreate(&stream);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    // Launch low-level CUDA kernel
+    cuda<T>(stream, val, eps, nelems, dev_nom, dev_denom, dev_src);
+    cuda_err = cudaStreamSynchronize(stream);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    // Copy result and deallocate device memory
+    cuda_err = cudaMemcpy(&src[0], dev_src, sizeof(T)*nelems,
+            cudaMemcpyDeviceToHost);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaFree(dev_src);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaFree(dev_nom);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaFree(dev_denom);
+    TEST_ASSERT(cuda_err == cudaSuccess);
+    cuda_err = cudaStreamDestroy(stream);
+    TEST_ASSERT(cuda_err == cudaSuccess);
 }
 #endif // NNTILE_USE_CUDA
 
@@ -85,29 +93,14 @@ void validate(T val, T eps, Index nelems)
     std::cout << "OK: kernel::addcdiv::cpu<T>\n";
 #ifdef NNTILE_USE_CUDA
     // Check low-level CUDA kernel
-    // dst = dst_save;
-    // std::cout << "Run kernel::addcdiv::cuda<T>\n";
-    // run_cuda<T>(nelems, src, dst);
-    // for(Index i = 0; i < nelems; ++i)
-    // {
-    //     T x = dst_save[i];
-    //     T val_ref = T((2*i+1-nelems)*(nelems-i)) / T{1000000};
-    //     // Obtain range of correct values
-    //     T val_ref_min, val_ref_max;
-    //     if(val_ref < 0)
-    //     {
-    //         val_ref_min = val_ref * (T{1}+eps) - eps;
-    //         val_ref_max = val_ref * (T{1}-eps) + eps;
-    //     }
-    //     else
-    //     {
-    //         val_ref_min = val_ref * (T{1}-eps) - eps;
-    //         val_ref_max = val_ref * (T{1}+eps) + eps;
-    //     }
-    //     // NaN-aware comparisons
-    //     TEST_ASSERT(dst[i] >= val_ref_min and dst[i] <= val_ref_max);
-    // }
-    // std::cout << "OK: kernel::addcdiv::cuda<T>\n";
+    src = src_copy;
+    std::cout << "Run kernel::addcdiv::cuda<T>\n";
+    run_cuda<T>(val, eps, nelems, &nom[0], &denom[0], &src[0]);
+    for(Index i = 0; i < nelems; ++i)
+    {
+        TEST_ASSERT(src[i] == (src_copy[i] + val * nom[i] / (denom[i] + eps)));
+    }
+    std::cout << "OK: kernel::addcdiv::cuda<T>\n";
 #endif // NNTILE_USE_CUDA
 }
 
