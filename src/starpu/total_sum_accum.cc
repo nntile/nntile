d7a93857ca1e3e3fb4fc14dafa75f4f1925699f3
Aleksandr Katrutsa <a.katrutsa@skoltech.ru>
2023-06-30 10:59:59 +0300

    Add cuda kernel for total_sum_accum

--- src/starpu/total_sum_accum.cc
+++ src/starpu/total_sum_accum.cc
@@ -23,7 +23,7 @@ namespace starpu
 namespace total_sum_accum
 {
 
-//! Max and sum of exponents along middle axis of StarPU buffer on CPU
+//! total_sum_accumulation operation of StarPU buffer on CPU
 template<typename T>
 void cpu(void *buffers[], void *cl_args)
     noexcept
@@ -44,7 +44,27 @@ void cpu(void *buffers[], void *cl_args)
 }
 
 #ifdef NNTILE_USE_CUDA
-
+//! Apply total_sum_accum operation on StarPU buffer on CUDA
+template<typename T>
+void cuda(void *buffers[], void *cl_args)
+    noexcept
+{
+    // Get arguments
+    auto args = reinterpret_cast<args_t *>(cl_args);
+    Index n_labels = args->n_labels;
+    Index n_outputs = args->n_outputs;
+    // Get interfaces
+    auto interfaces = reinterpret_cast<VariableInterface **>(buffers);
+    const T *logsumexp = interfaces[0]->get_ptr<T>();
+    const T *src = interfaces[1]->get_ptr<T>();
+    const Index* labels = interfaces[2]->get_ptr<Index>();
+    T* val = interfaces[3]->get_ptr<T>();
+    // Get CUDA stream
+    cudaStream_t stream = starpu_cuda_get_local_stream();
+    // Launch kernel
+    kernel::total_sum_accum::cuda<T>(stream, n_labels, n_outputs, logsumexp, src,
+            labels, val);
+}
 #endif // NNTILE_USE_CUDA
 
 Codelet codelet_fp32, codelet_fp64;
@@ -55,8 +75,7 @@ void init()
             nullptr,
             {cpu<fp32_t>},
 #ifdef NNTILE_USE_CUDA
-            // {cuda<fp32_t>}
-            {}
+            {cuda<fp32_t>}
 #else // NNTILE_USE_CUDA
             {}
 #endif // NNTILE_USE_CUDA
@@ -65,8 +84,7 @@ void init()
             nullptr,
             {cpu<fp64_t>},
 #ifdef NNTILE_USE_CUDA
-            // {cuda<fp64_t>}
-            {}
+            {cuda<fp64_t>}
 #else // NNTILE_USE_CUDA
             {}
 #endif // NNTILE_USE_CUDA
