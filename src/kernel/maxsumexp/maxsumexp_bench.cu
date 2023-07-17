23cdc9ff95ae62a7f6f17bf5d4148c02dad67b0d
Daniel Bershatsky <d.bershatsky2@skoltech.ru>
2023-07-17 21:01:17 +0300

    Add microbenchmark of copying in maxsumexp

--- src/kernel/maxsumexp/maxsumexp_bench.cu
+++ src/kernel/maxsumexp/maxsumexp_bench.cu
@@ -6,6 +6,8 @@
 #include "nntile/kernel/maxsumexp.hh"
 #include "nntile/kernel/maxsumexp/cuda.hh"
 
+using nntile::Index;
+
 namespace maxsumexp = nntile::kernel::maxsumexp;
 
 enum class Device : int {
@@ -43,6 +45,44 @@ template <typename T> struct Array<T, Device::kCUDA> {
         return reinterpret_cast<U const *>(data);
     }
 };
+
+template <typename T>
+__global__ void Copy(Index n, Index m, Index k, T const *src, T *dst) {
+    auto ix = threadIdx.y + blockIdx.y * blockDim.y;
+    auto jx = threadIdx.x + blockIdx.x * blockDim.x;
+    auto kx = threadIdx.z + blockIdx.z * blockDim.z;
+    if (ix >= n || kx >= m) {
+        return;
+    }
+    for (auto jt = 0; jt != k; ++jt) {
+        auto value = src[ix + n * (jx + jt) + n * m * kx];
+        dst[ix + n * kx + 0] = value;
+        dst[ix + n * kx + 1] = value;
+    }
+}
+
+void BenchCopy(nvbench::state &state) {
+    auto batch_size = static_cast<int>(state.get_int64("batch_size"));
+    auto seq_len = static_cast<int>(state.get_int64("seq_len"));
+    auto src = Array<float, Device::kCUDA>(batch_size * seq_len * seq_len);
+    auto dst = Array<float, Device::kCUDA>(batch_size * seq_len * 2);
+
+    // Request throughput stats.
+    state.add_element_count(src.size);
+    state.add_global_memory_reads<float>(src.size);
+    state.add_global_memory_writes<float>(dst.size);
+    state.exec(nvbench::exec_tag::sync, [&](nvbench::launch &launch) {
+        dim3 threads(256);
+        dim3 blocks(1, batch_size, seq_len);
+        Copy<float><<<threads, blocks, 0, launch.get_stream()>>>(
+            batch_size, seq_len, seq_len, src.as<float>(), dst.as<float>());
+        cudaStreamSynchronize(launch.get_stream());
+    });
+}
+
+NVBENCH_BENCH(BenchCopy)
+    .add_int64_axis("batch_size", {2, 8, 32})
+    .add_int64_axis("seq_len", {64, 256});
 
 void BenchMaxSumExp(nvbench::state &state) {
     auto batch_size = static_cast<int>(state.get_int64("batch_size"));
