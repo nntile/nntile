c556a07b45f1d60962efcca5d95c7f40e33202ec
Daniel Bershatsky <d.bershatsky2@skoltech.ru>
2023-07-20 09:09:55 +0300

    Test maxsumexp with GTest

--- tests/kernel/maxsumexp.cc
+++ tests/kernel/maxsumexp.cc
@@ -12,162 +12,194 @@
  * @date 2022-12-07
  * */
 
-#include "nntile/kernel/maxsumexp.hh"
-#include "../testing.hh"
-#include <vector>
-#include <stdexcept>
-#include <limits>
+#include <algorithm>
 #include <cmath>
-#include <iostream>
+#include <limits>
+#include <stdexcept>
+#include <strings.h>
+#include <tuple>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "nntile/kernel/maxsumexp.hh"
 
 using namespace nntile;
 using namespace nntile::kernel::maxsumexp;
 
-#ifdef NNTILE_USE_CUDA
-template<typename T>
-void run_cuda(Index m, Index n, Index k, const std::vector<T> &src,
-        std::vector<T> &maxsumexp)
-{
-    // Copy to device
-    T *dev_src, *dev_maxsumexp;
-    cudaError_t cuda_err = cudaMalloc(&dev_src, sizeof(T)*m*n*k);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    cuda_err = cudaMalloc(&dev_maxsumexp, sizeof(T)*2*m*n);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    cuda_err = cudaMemcpy(dev_src, &src[0], sizeof(T)*m*n*k,
-            cudaMemcpyHostToDevice);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    cuda_err = cudaMemcpy(dev_maxsumexp, &maxsumexp[0], sizeof(T)*2*m*n,
-            cudaMemcpyHostToDevice);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    // Init stream
-    cudaStream_t stream;
-    cuda_err = cudaStreamCreate(&stream);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    // Launch low-level kernel
-    cuda<T>(stream, m, n, k, dev_src, dev_maxsumexp);
-    cuda_err = cudaStreamSynchronize(stream);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    // Copy result and deallocate device memory
-    cuda_err = cudaMemcpy(&maxsumexp[0], dev_maxsumexp, sizeof(T)*2*m*n,
-            cudaMemcpyDeviceToHost);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    cuda_err = cudaFree(dev_src);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    cuda_err = cudaFree(dev_maxsumexp);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-    cuda_err = cudaStreamDestroy(stream);
-    TEST_ASSERT(cuda_err == cudaSuccess);
-}
-#endif // NNTILE_USE_CUDA
+// TileShape (aka batch size, reduced size, seq len) represents a shape of tile
+// which is reduced along the middle (seq len) axis.
+using TileShape = std::tuple<int, int, int>;
 
-// Templated validation
-template<typename T>
-void validate(Index m, Index n, Index k)
-{
-    constexpr T eps = std::numeric_limits<T>::epsilon();
-    // Init test input
-    std::vector<T> src(m*n*k), maxsumexp(2*m*n);
-    for(Index i0 = 0; i0 < m; ++i0)
-    {
-        for(Index i1 = 0; i1 < n; ++i1)
-        {
-            for(Index i2 = 0; i2 < k; ++i2)
-            {
-                src[(i1*k+i2)*m+i0] = T(i0+i1+i2) / T{10};
+class MaxSumExpCPU : public ::testing::TestWithParam<TileShape> {
+protected:
+    void SetUp(void) override {
+        batch_size_ = std::get<0>(GetParam());
+        seq_len_ = std::get<1>(GetParam());
+        reduced_size_ = std::get<2>(GetParam());
+    }
+
+    template <typename T> std::vector<T> GenerateData(void) {
+        Index m = batch_size_, n = seq_len_, k = reduced_size_;
+        std::vector<T> src(m * n * k);
+        for (Index i0 = 0; i0 < m; ++i0) {
+            for (Index i1 = 0; i1 < n; ++i1) {
+                for (Index i2 = 0; i2 < k; ++i2) {
+                    src[(i1 * k + i2) * m + i0] = T(i0 + i1 + i2) / T{10};
+                }
+            }
+        }
+        return src;
+    }
+
+    template <typename T> void AssertSimple(std::vector<T> const &maxsumexp) {
+        T constexpr eps = std::numeric_limits<T>::epsilon();
+        Index m = batch_size_, n = seq_len_, k = reduced_size_;
+        for (Index i0 = 0; i0 < m; ++i0) {
+            for (Index i1 = 0; i1 < n; ++i1) {
+                Index a = i0 + i1;
+                T max_ref = T(a + k - 1) / T{10};
+                T max = maxsumexp[2 * (i1 * m + i0)];
+                ASSERT_EQ(max, max_ref);
+                T sum_ref = 0;
+                for (Index i2 = 0; i2 < k; ++i2) {
+                    sum_ref += std::exp(T(i2 - k + 1) / T{10});
+                }
+                T sum = maxsumexp[2 * (i1 * m + i0) + 1];
+                auto rerr = std::abs(sum / sum_ref - T{1});
+                ASSERT_TRUE(rerr <= 10 * eps)
+                    << sum << " vs " << sum_ref << " at (" << i0 << ", " << i1
+                    << ")";
             }
         }
     }
-    std::vector<T> maxsumexp_copy(maxsumexp);
-    // Check low-level kernel
-    std::cout << "Run kernel::maxsumexp::cpu<T>\n";
-    cpu<T>(m, n, k, &src[0], &maxsumexp[0]);
-    for(Index i0 = 0; i0 < m; ++i0)
-    {
-        for(Index i1 = 0; i1 < n; ++i1)
-        {
-            Index a = i0 + i1;
-            T max_ref = T(a + k - 1) / T{10};
-            T max = maxsumexp[2*(i1*m+i0)];
-            TEST_ASSERT(max == max_ref);
-            T sum_ref = 0;
-            for(Index i2 = 0; i2 < k; ++i2)
-            {
-                sum_ref += std::exp(T(i2-k+1)/T{10});
+
+    template <typename T>
+    void AssertDetailed(std::vector<T> const &maxsumexp,
+                        std::vector<T> const &maxsumexp_copy) {
+        T constexpr eps = std::numeric_limits<T>::epsilon();
+        Index m = batch_size_, n = seq_len_, k = reduced_size_;
+        for (Index i0 = 0; i0 < m; ++i0) {
+            for (Index i1 = 0; i1 < n; ++i1) {
+                Index i = 2 * (i1 * m + i0);
+                ASSERT_EQ(maxsumexp[i], maxsumexp_copy[i]);
+                auto rerr =
+                    std::abs(maxsumexp[i + 1] / maxsumexp_copy[i + 1] - T{2});
+                ASSERT_TRUE(rerr <= 10 * eps)
+                    << maxsumexp[i + 1] << " vs " << maxsumexp_copy[i + 1]
+                    << " at (" << i0 << ", " << i1 << ")";
             }
-            T sum = maxsumexp[2*(i1*m+i0)+1];
-            TEST_ASSERT(std::abs(sum/sum_ref-T{1}) <= 10*eps);
         }
     }
-    std::cout << "OK: kernel::maxsumexp::cpu<T>\n";
-#ifdef NNTILE_USE_CUDA
-    // Check low-level CUDA kernel
-    maxsumexp = maxsumexp_copy;
-    std::cout << "Run kernel::maxsumexp::cuda<T>\n";
-    run_cuda<T>(m, n, k, src, maxsumexp);
-    for(Index i0 = 0; i0 < m; ++i0)
-    {
-        for(Index i1 = 0; i1 < n; ++i1)
-        {
-            Index a = i0 + i1;
-            T max_ref = T(a + k - 1) / T{10};
-            T max = maxsumexp[2*(i1*m+i0)];
-            TEST_ASSERT(max == max_ref);
-            T sum_ref = 0;
-            for(Index i2 = 0; i2 < k; ++i2)
-            {
-                sum_ref += std::exp(T(i2-k+1)/T{10});
-            }
-            T sum = maxsumexp[2*(i1*m+i0)+1];
-            TEST_ASSERT(std::abs(sum/sum_ref-T{1}) <= 10*eps);
-        }
+
+    template <typename T> void RunTest(void) {
+        Index m = batch_size_, n = seq_len_, k = reduced_size_;
+
+        std::vector<T> src = GenerateData<T>();
+        std::vector<T> maxsumexp(2 * batch_size_ * seq_len_);
+        cpu<T>(m, n, k, &src[0], &maxsumexp[0]);
+        AssertSimple<T>(maxsumexp);
+
+        std::vector<T> maxsumexp_copy(maxsumexp);
+        cpu<T>(m, n, k, &src[0], &maxsumexp[0]);
+        AssertDetailed<T>(maxsumexp, maxsumexp_copy);
     }
-    std::cout << "OK: kernel::maxsumexp::cuda<T>\n";
-#endif // NNTILE_USE_CUDA
-    // Check low-level kernel even more
-    maxsumexp_copy = maxsumexp;
-    std::cout << "Run kernel::maxsumexp::cpu<T>\n";
-    cpu<T>(m, n, k, &src[0], &maxsumexp[0]);
-    for(Index i0 = 0; i0 < m; ++i0)
-    {
-        for(Index i1 = 0; i1 < n; ++i1)
-        {
-            Index i = 2 * (i1*m+i0);
-            TEST_ASSERT(maxsumexp[i] == maxsumexp_copy[i]);
-            TEST_ASSERT(std::abs(maxsumexp[i+1]/maxsumexp_copy[i+1]
-                        -T{2}) <= 10*eps);
-        }
-    }
-    std::cout << "OK: kernel::maxsumexp::cpu<T>\n";
-#ifdef NNTILE_USE_CUDA
-    // Check low-level CUDA kernel
-    maxsumexp = maxsumexp_copy;
-    std::cout << "Run kernel::maxsumexp::cuda<T>\n";
-    run_cuda<T>(m, n, k, src, maxsumexp);
-    for(Index i0 = 0; i0 < m; ++i0)
-    {
-        for(Index i1 = 0; i1 < n; ++i1)
-        {
-            Index i = 2 * (i1*m+i0);
-            TEST_ASSERT(maxsumexp[i] == maxsumexp_copy[i]);
-            TEST_ASSERT(std::abs(maxsumexp[i+1]/maxsumexp_copy[i+1]
-                        -T{2}) <= 10*eps);
-        }
-    }
-    std::cout << "OK: kernel::maxsumexp::cuda<T>\n";
-#endif // NNTILE_USE_CUDA
+
+protected:
+    int batch_size_;
+    int reduced_size_;
+    int seq_len_;
+};
+
+TEST_P(MaxSumExpCPU, FP64) {
+    RunTest<double>();
 }
 
-int main(int argc, char **argv)
-{
-    validate<fp32_t>(1, 9, 10);
-    validate<fp32_t>(8, 9, 1);
-    validate<fp32_t>(8, 1, 10);
-    validate<fp32_t>(4, 7, 8);
-    validate<fp64_t>(1, 9, 10);
-    validate<fp64_t>(8, 9, 1);
-    validate<fp64_t>(8, 1, 10);
-    validate<fp64_t>(4, 7, 8);
-    return 0;
+TEST_P(MaxSumExpCPU, FP32) {
+    RunTest<float>();
 }
 
+INSTANTIATE_TEST_SUITE_P(Kernel, MaxSumExpCPU,
+                         ::testing::Values(std::make_tuple(1, 9, 10),
+                                           std::make_tuple(8, 9, 1),
+                                           std::make_tuple(8, 1, 10),
+                                           std::make_tuple(4, 7, 8),
+                                           std::make_tuple(32, 1024, 1024)));
+
+template <typename T> void FreeDeviceArray(T *ptr) {
+    if (ptr) {
+        cudaFree(ptr);
+    }
+};
+
+template <typename T>
+using device_ptr = std::unique_ptr<T, decltype(&FreeDeviceArray<T>)>;
+
+template <typename T>
+device_ptr<T> MakeDevicePtrLike(std::vector<T> const &src) {
+    T *ptr = nullptr;
+    cudaMalloc(&ptr, src.size() * sizeof(T));
+    return device_ptr<T>(ptr, FreeDeviceArray);
+}
+
+class MaxSumExpCUDA : public MaxSumExpCPU {
+protected:
+    cudaStream_t stream_;
+
+protected:
+    void SetUp(void) override {
+        MaxSumExpCPU::SetUp();
+        if (cudaStreamCreate(&stream_) != cudaSuccess) {
+            // TODO(@bershatsky): Handle CUDA errors.
+        }
+    }
+
+    void TearDown(void) override {
+        cudaStreamDestroy(stream_);
+        MaxSumExpCPU::TearDown();
+    }
+
+    template <typename T>
+    void LaunchKernel(Index m, Index n, Index k, T const *input, T *output,
+                      std::vector<T> &result) {
+        cuda<T>(stream_, m, n, k, input, output);
+        ASSERT_EQ(cudaStreamSynchronize(stream_), cudaSuccess);
+        ASSERT_EQ(cudaGetLastError(), cudaSuccess);
+        cudaMemcpy(result.data(), output, result.size() * sizeof(T),
+                   cudaMemcpyDeviceToHost);
+    }
+
+    template <typename T> void RunTest(void) {
+        Index m = batch_size_, n = seq_len_, k = reduced_size_;
+
+        std::vector<T> src = GenerateData<T>();
+        std::vector<T> maxsumexp(2 * batch_size_ * seq_len_);
+        auto source_ = MakeDevicePtrLike(src);
+        auto target_ = MakeDevicePtrLike(maxsumexp);
+        cudaMemcpy(source_.get(), src.data(), src.size() * sizeof(T),
+                   cudaMemcpyHostToDevice);
+        cudaMemcpy(target_.get(), maxsumexp.data(),
+                   maxsumexp.size() * sizeof(T), cudaMemcpyHostToDevice);
+        LaunchKernel(m, n, k, source_.get(), target_.get(), maxsumexp);
+        AssertSimple<T>(maxsumexp);
+
+        std::vector<T> maxsumexp_copy(maxsumexp);
+        LaunchKernel(m, n, k, source_.get(), target_.get(), maxsumexp);
+        AssertDetailed<T>(maxsumexp, maxsumexp_copy);
+    }
+};
+
+TEST_P(MaxSumExpCUDA, FP64) {
+    RunTest<double>();
+}
+
+TEST_P(MaxSumExpCUDA, FP32) {
+    RunTest<float>();
+}
+
+INSTANTIATE_TEST_SUITE_P(Kernel, MaxSumExpCUDA,
+                         ::testing::Values(std::make_tuple(1, 9, 10),
+                                           std::make_tuple(8, 9, 1),
+                                           std::make_tuple(8, 1, 10),
+                                           std::make_tuple(4, 7, 8),
+                                           std::make_tuple(32, 1024, 1024)));
