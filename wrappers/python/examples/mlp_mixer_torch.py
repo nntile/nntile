7e2cceef9bd1854556d33ba579b08aaa3311a0c5
g.karpov <g.karpov@skoltech.ru>
2023-11-20 01:52:00 +0300

    pipeline for training torch mlp_mixer model

--- wrappers/python/examples/mlp_mixer_torch.py
+++ wrappers/python/examples/mlp_mixer_torch.py
@@ -17,7 +17,6 @@ import torchvision.datasets as dts
 import numpy as np
 import torchvision.transforms as trnsfrms
 from nntile.torch_models.mlp_mixer import MlpMixer, image_patching_rgb
-from nntile.model.mlp_mixer import MlpMixer as MlpMixerTile
 
 device = (
     "cuda"
@@ -27,7 +26,10 @@ device = (
 print(f"Using {device} device")
 
 patch_size = 16
-batch_size = 8
+minibatch_size = 16
+batch_size = 512
+accumulation_steps = int(batch_size / minibatch_size)
+
 channel_size = int(32 * 32 / patch_size ** 2)
 hidden_dim = 1024
 num_mixer_layers = 8
@@ -35,10 +37,7 @@ num_classes = 10
 num_clr_channels = 3
 
 lr = 1e-3
-stop_train_iter = 10000
-next_tag = 0
-
-X_shape = [channel_size, batch_size, patch_size]
+num_epochs = 10
 
 mlp_mixer_model = MlpMixer(channel_size, num_clr_channels * patch_size ** 2, hidden_dim, num_mixer_layers, num_classes).to(device)
 optim_torch = torch.optim.SGD(mlp_mixer_model.parameters(), lr=lr)
@@ -46,31 +45,42 @@ crit_torch = nn.CrossEntropyLoss(reduction="sum")
 
 trnsform = trnsfrms.Compose([trnsfrms.ToTensor()])
 
-cifartrainset = dts.CIFAR10(root='/datasets/cifar10/', train=True, download=False, transform=trnsform)
-trainldr = torch.utils.data.DataLoader(cifartrainset, batch_size=batch_size, shuffle=True)
+cifartrainset = dts.CIFAR10(root='/mnt/local/dataset/by-domain/cv/CIFAR10/', train=True, download=False, transform=trnsform)
+trainldr = torch.utils.data.DataLoader(cifartrainset, batch_size=minibatch_size, shuffle=True)
+total_train_samples = len(trainldr)
 
-training_iteration = 0
+minibatch_counter = 0
 interm_loss = []
+
 mlp_mixer_model.zero_grad()
-for train_batch_sample, true_labels in trainldr:
-    # print(true_labels)
-    # train_batch_sample = train_batch_sample.view(-1, 32, 32)
-    # train_batch_sample = torch.swapaxes(train_batch_sample, 0, 1)
-    patched_train_sample = image_patching_rgb(train_batch_sample, patch_size)
-    print(patched_train_sample.device)
-    mlp_mixer_model.zero_grad()
-    torch_output = mlp_mixer_model(patched_train_sample)
-    torch_loss = crit_torch(torch_output, true_labels)
-    torch_loss.backward()
-    print("Intermediate PyTorch loss =", torch_loss.item())
-    if (training_iteration % 50) == 49:
-        print("Intermediate PyTorch loss =", torch_loss.item())
-        interm_loss.append(torch_loss.item())
-    optim_torch.step()
 
-    if training_iteration == stop_train_iter:
-            break    
-    training_iteration += 1
+torch.save({
+            'model_state_dict': mlp_mixer_model.state_dict(),
+            'optimizer_state_dict': optim_torch.state_dict(),
+            }, 'init_state.pt')
 
+for epoch_counter in range(num_epochs):  
+    for train_iter, (train_batch_sample, true_labels) in enumerate(trainldr):
+        patched_train_sample = image_patching_rgb(train_batch_sample, patch_size)
+        patched_train_sample = patched_train_sample.to(device)
+        true_labels = true_labels.to(device)
+ 
+        torch_output = mlp_mixer_model(patched_train_sample)
+        torch_loss = crit_torch(torch_output, true_labels)
+        # normalize loss to account for batch accumulation
+        torch_loss = torch_loss / accumulation_steps
+        interm_loss.append(torch_loss.to('cpu').detach().numpy())
+
+        torch_loss.backward()
+        if (train_iter + 1) % accumulation_steps == 0 or (train_iter + 1) == total_train_samples:
+            optim_torch.step()
+            mlp_mixer_model.zero_grad() 
+        
+torch.save({
+            'epoch': num_epochs,
+            'model_state_dict': mlp_mixer_model.state_dict(),
+            'optimizer_state_dict': optim_torch.state_dict(),
+            'loss': interm_loss,
+            }, 'final_state.pt')
 np.savez('Loss_cifar10', loss=interm_loss)
 
