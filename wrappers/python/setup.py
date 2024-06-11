647a17c15de632797422e7c13c0116f2c5c6a1bb
Daniel Bershatsky <d.bershatsky2@skoltech.ru>
2024-06-12 01:52:06 +0300

    Rework building Python extension library
    This commit solves the issue of installation of Python extension library
    to specific directory for packaging or in-tree development and testing.
    However, the issue is still persist for building a wheel. Specifically,
    the following attempt to build a wheel distribution fails.
    
    ```shell
    python -m build -nvw -C --build-temp=build
    ```
    
    The pre vious command is run in repository root directory and fails
    since build directory is out of Python package root directory (i.e.
    `wrappers/python`). Thus the simplest solution is to match Python
    package root with repository root.

---
+++ wrappers/python/setup.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+class CMakeExtension(Extension):
+
+    def __init__(self, name):
+        super().__init__(name, sources=[])
+
+
+class build_ext(_build_ext):
+
+    def run(self):
+        cmake_extensions = []
+        rest_extensions = []
+        for ext in self.extensions:
+            if isinstance(ext, CMakeExtension):
+                cmake_extensions.append(ext)
+            else:
+                rest_extensions.append(ext)
+        self.extensions = rest_extensions
+        super().run()
+        for ext in cmake_extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        current_dir = Path.cwd()
+        package_dir = Path(__file__).parent
+        root_dir = (package_dir / '../..').resolve().relative_to(current_dir)
+
+        build_dir = Path(self.build_temp)
+        install_dir = Path(self.build_lib)
+        if self.inplace:
+            install_dir = (package_dir / 'nntile').relative_to(current_dir)
+
+        # Reconfigure cmake with directory to "install" extension library.
+        cmd = ('cmake', '-S', str(root_dir), '-B', str(build_dir),
+               f'-DNNTILE_CORE_SYMLINK_DIR={install_dir}')
+        self.spawn(cmd)
+
+        # Make a symlink to `nntile_core` shared library.
+        cmd = ('cmake', '--build', str(build_dir), '--target',
+               'nntile-symlink')
+        self.spawn(cmd)
+
+    def spawn(self, cmd, search_path=1, level=1):
+        if self.verbose:
+            command = ' '.join(cmd)
+            print(f'build_ext: run command: ({command})')
+        super().spawn(cmd, search_path, level)
+
+setup(ext_modules=[CMakeExtension('nntile.nntile_core')],
+      cmdclass={'build_ext': build_ext})
