d939afb86cad2313eac88e17b8d7da346f238b65
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2024-06-19 13:57:21 +0300

    Update license header in tests

--- tests/tensor/add_fiber.cc
+++ tests/tensor/add_fiber.cc
@@ -1,5 +1,7 @@
-/*! @copyright (c) 2022-2023 Skolkovo Institute of Science and Technology
- *                           (Skoltech). All rights reserved.
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
  *
  * NNTile is software framework for fast training of big neural networks on
  * distributed-memory heterogeneous systems based on StarPU runtime system.
@@ -8,8 +10,6 @@
  * Tensor wrappers for addition of a tensor and a broadcasted fiber
  *
  * @version 1.0.0
- * @author Aleksandr Mikhalev
- * @date 2023-12-18
  * */
 
 #include "nntile/tensor/add_fiber.hh"
--- tests/tensor/sqrt_inplace.cc
+++ tests/tensor/sqrt_inplace.cc
@@ -1,5 +1,7 @@
-/*! @copyright (c) 2022-2023 Skolkovo Institute of Science and Technology
- *                           (Skoltech). All rights reserved.
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
  *
  * NNTile is software framework for fast training of big neural networks on
  * distributed-memory heterogeneous systems based on StarPU runtime system.
@@ -8,9 +10,6 @@
  * sqrt_inplace operation for Tensor<T>
  *
  * @version 1.0.0
- * @author Aleksandr Katrutsa
- * @author Aleksandr Mikhalev
- * @date 2023-12-18
  * */
 
 #include "nntile/tensor/sqrt_inplace.hh"
