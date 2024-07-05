1e84cc1500933ffadaf5b72c3510af8115d52bbe
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2024-07-05 19:51:24 +0000

    RM base_types.cc; add CUDA conversion into base_types.hh

--- src/kernel/accumulate_maxsumexp/cuda.cu
+++ src/kernel/accumulate_maxsumexp/cuda.cu
@@ -20,7 +20,7 @@ namespace nntile::kernel::accumulate_maxsumexp
 
 template<typename T>
 static __global__
-void cuda_kernel(Index nelems, const T *src_, T *dst_)
+void cuda_kernel(Index nelems, const T *src, T *dst)
 //! Accumulate two maxsumexp buffers on CUDA
 /*! Performs the following operation:
  *      dst[2*i+1] = dst[2*i+1]*exp(dst[2*i]) + src[2*i+1]*exp(src[2*i]),
@@ -33,17 +33,14 @@ void cuda_kernel(Index nelems, const T *src_, T *dst_)
 {
     int i = threadIdx.x + blockIdx.x*blockDim.x;
     using Y = typename T::repr_t;
-    using Z = typename CUDAComputeType<T>::value;
-    auto src = reinterpret_cast<const Z *>(src_);
-    auto dst = reinterpret_cast<Z *>(dst_);
     constexpr Y zero = 0.0;
-    Y dst_odd = Y{dst[2*i+1]};
-    Y dst_even = Y{dst[2*i]};
-
-    Y src_odd = Y{src[2*i+1]};
-    Y src_even = Y{src[2*i]};
     if(i < nelems)
     {
+        Y dst_odd(dst[2*i+1]);
+        Y dst_even(dst[2*i]);
+
+        Y src_odd(src[2*i+1]);
+        Y src_even(src[2*i]);
         // Do nothing if sum of exponents of source is zero
         if(src_odd != zero)
         {
@@ -56,19 +53,19 @@ void cuda_kernel(Index nelems, const T *src_, T *dst_)
             // Otherwise update based on maximum
             else if(dst_even < src_even)
             {
-                dst[2*i+1] = Z{src_odd + dst_odd * ::exp(dst_even - src_even)};
+                dst[2*i+1] = src_odd + dst_odd*::exp(dst_even-src_even);
                 dst[2*i] = src[2*i];
             }
             else
             {
-                dst[2*i+1] = Z{dst_odd + src_odd * ::exp(src_even - dst_even)};            
+                dst[2*i+1] = dst_odd + src_odd*::exp(src_even-dst_even);
             }
         }
     }
 }
 
 template<typename T>
-void cuda(cudaStream_t stream, Index nelems, const T *src_, T *dst_)
+void cuda(cudaStream_t stream, Index nelems, const T *src, T *dst)
     noexcept
 //! Accumulate two maxsumexp buffers on CUDA
 /*! Performs the following operation:
@@ -76,15 +73,12 @@ void cuda(cudaStream_t stream, Index nelems, const T *src_, T *dst_)
  *      dst[2*i] = max(src[2*i], dst[2*i]).
  *
  * @param[in] nelems: Number of (max,sumexp) pairs of the src and dst tensors
- * @param[in] src_: Source tensor
- * @param[inout] dst_: Destination of the maxsumexp accumulation
+ * @param[in] src: Source tensor
+ * @param[inout] dst: Destination of the maxsumexp accumulation
  * */
 {
     dim3 blocks((nelems+255)/256), threads(256);
-    // auto src = cast_pointer_cuda<T>(src_);
-    // auto dst = cast_pointer_cuda<T>(dst_);
-    // using Y = typename CUDAComputeType<T>::value;
-    (cuda_kernel<T>)<<<blocks, threads, 0, stream>>>(nelems, src_, dst_);
+    (cuda_kernel<T>)<<<blocks, threads, 0, stream>>>(nelems, src, dst);
 }
 
 // Explicit instantiation
