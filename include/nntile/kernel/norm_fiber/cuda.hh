ff5894b95c555eabfc7329b566af8a00a5ce3b2d
gogoglrind <gogolgrind@gmail.com>
2024-07-16 15:35:15 +0000

    update include files

---
+++ include/nntile/kernel/norm_fiber/cuda.hh
@@ -0,0 +1,29 @@
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
+ *
+ * NNTile is software framework for fast training of big neural networks on
+ * distributed-memory heterogeneous systems based on StarPU runtime system.
+ *
+ * @file include/nntile/kernel/norm_fiber/cuda.hh
+ * Euclidean norms over slices into a fiber of a product of a buffer on CUDA
+ *
+ * @version 1.0.0
+ * */
+
+#pragma once
+
+#include <nntile/base_types.hh>
+#include <cuda_runtime.h>
+
+namespace nntile::kernel::norm_fiber
+{
+
+// Euclidean norms over slices into a fiber of a product of buffers on GPU
+template<typename T>
+void cuda(cudaStream_t stream, Index m, Index n, Index k, Index batch, Scalar alpha,
+        const T *src, Scalar beta, T *dst)
+    noexcept;
+
+} // namespace nntile::kernel::norm_fiber
---
+++ include/nntile/starpu/norm_fiber.hh
@@ -0,0 +1,82 @@
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
+ *
+ * NNTile is software framework for fast training of big neural networks on
+ * distributed-memory heterogeneous systems based on StarPU runtime system.
+ *
+ * @file include/nntile/starpu/norm_fiber.hh
+ * Euclidean norms over slices into a fiber of a product of a StarPU buffer
+ *
+ * @version 1.0.0
+ * */
+
+#pragma once
+
+#include <nntile/base_types.hh>
+#include <nntile/starpu/config.hh>
+
+namespace nntile::starpu::norm_fiber
+{
+
+//! Structure for arguments
+struct args_t
+{
+    Index m;
+    Index n;
+    Index k;
+    Index batch;
+    Scalar alpha;
+    Scalar beta;
+};
+
+// StarPU wrapper for kernel::norm_fiber::cpu<T>
+template<typename T>
+void cpu(void *buffers[], void *cl_args)
+    noexcept;
+
+extern Codelet codelet_fp32, codelet_fp64, codelet_fp32_fast_tf32, codelet_bf16;
+
+template<typename T>
+constexpr Codelet *codelet()
+{
+    throw std::runtime_error("Non-supported type");
+    return nullptr;
+}
+
+template<>
+constexpr Codelet *codelet<fp32_t>()
+{
+    return &codelet_fp32;
+}
+
+template<>
+constexpr Codelet *codelet<bf16_t>()
+{
+    return &codelet_bf16;
+}
+
+template<>
+constexpr Codelet *codelet<fp32_fast_tf32_t>()
+{
+    return &codelet_fp32_fast_tf32;
+}
+
+template<>
+constexpr Codelet *codelet<fp64_t>()
+{
+    return &codelet_fp64;
+}
+
+void init();
+
+void restrict_where(uint32_t where);
+
+void restore_where();
+
+template<typename T>
+void submit(Index m, Index n, Index k, Index batch, Scalar alpha, Handle src,
+        Scalar beta, Handle dst, int redux=0);
+
+} // namespace nntile::starpu::norm_fiber
