767ca19c43f70ea7ff2faf75d9758b16d181af5e
Daniel Bershatsky <d.bershatsky2@skoltech.ru>
2024-07-17 17:45:58 +0300

    Remove HuggingFace cache dir fixture
    In order to override HuggingFace assets directory, one should use
    `HF_HOME`, `HF_HUB_CACHE` or other specific environment variable (see
    https://huggingface.co/docs/huggingface_hub/package_reference/environment_variables
    for details).

--- wrappers/python/tests/conftest.py
+++ wrappers/python/tests/conftest.py
@@ -1,4 +1,3 @@
-import os
 from typing import Generator
 
 import pytest
@@ -46,9 +45,3 @@ def torch_rng():
     gen.manual_seed(seed)
 
     return gen
-
-
-@pytest.fixture(scope="package")
-def huggingface_local_cache_dir():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    return f"{dir_path}/hf_cache"
--- wrappers/python/tests/model/test_generate.py
+++ wrappers/python/tests/model/test_generate.py
@@ -2,17 +2,11 @@ from dataclasses import dataclass
 
 import numpy as np
 import pytest
+from transformers import GPT2Tokenizer
 
 import nntile.utils.constructors as nnt_constructors
 from nntile.model.generation.llm import GenerationMode, GenerationParams
 from nntile.model.gpt2 import GPT2Model as GPT2Model_nnt
-
-
-def get_tokenizer(model_name, cache_dir):
-    from transformers import GPT2TokenizerFast
-
-    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2", cache_dir=cache_dir)
-    return tokenizer
 
 
 @dataclass
@@ -39,10 +33,8 @@ TEST_GENERATE_INPUT_PARAMS = [
 
 @pytest.mark.slow
 @pytest.mark.parametrize("params", TEST_GENERATE_INPUT_PARAMS)
-def test_generation_from_pretrained(
-    starpu_simple, huggingface_local_cache_dir, params
-):
-    tokenizer = get_tokenizer(params.model_name, huggingface_local_cache_dir)
+def test_generation_from_pretrained(starpu_simple, params):
+    tokenizer = GPT2Tokenizer.from_pretrained(params.model_name)
     next_tag = 0
     model_nnt, next_tag = GPT2Model_nnt.from_pretrained(
         params.model_name,
