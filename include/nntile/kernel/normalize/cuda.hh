726c4635fe8c46b1f3083b4b9affb3c96d85a77d
Daniel Bershatsky <d.bershatsky2@skoltech.ru>
2024-07-19 12:42:19 +0300

    Get rid of ambiguous variable name `l` (E741)

--- include/nntile/kernel/normalize/cuda.hh
+++ include/nntile/kernel/normalize/cuda.hh
@@ -21,8 +21,8 @@ namespace nntile::kernel::normalize
 {
 
 template<typename T>
-void cuda(cudaStream_t stream, Index m, Index n, Index k, Index l, Scalar eps,
-        const T *gamma, const T *beta, const T *sumnorm, T *dst)
+void cuda(cudaStream_t stream, Index m, Index n, Index k, Index size,
+        Scalar eps, const T *gamma, const T *beta, const T *sumnorm, T *dst)
     noexcept;
 
 } // namespace nntile::kernel::normalize
