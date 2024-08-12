c1d832a5ee7f8e3bb4e93b6ac4e02e0fc3684b0d
svtdanny <sivtsovdt@gmail.com>
2024-08-12 17:34:20 +0300

    add kvcache for llama attention

--- wrappers/python/tests/layer/test_llama_attention.py
+++ wrappers/python/tests/layer/test_llama_attention.py
@@ -15,6 +15,10 @@
 
 from dataclasses import dataclass
 
+from gen_utils import (
+    generate_greedy_logits_dynamic_kvcache,
+    generate_greedy_logits_padding,
+)
 import numpy as np
 import pytest
 import torch
@@ -22,6 +26,7 @@ from transformers.models.llama.modeling_llama import (
     LlamaAttention as LlamaAttention_torch, LlamaConfig as LlamaConfig_torch)
 
 import nntile
+import nntile.utils.constructors as nntc
 from nntile.model.llama_config import LlamaConfigNNTile
 from nntile.tensor import TensorMoments, TensorTraits
 from nntile.utils.constructors import to_numpy
@@ -55,6 +60,18 @@ class LlamaAttentionTestParams:
     n_head_kv: int
     layer_idx: int = 0
     theta = 2.0
+
+single_tile_trivial = LlamaAttentionTestParams(
+    n_emb=4,
+    n_emb_tile=4,
+    n_seq=10,
+    n_seq_tile=10,
+    n_batch=1,
+    n_batch_tile=1,
+    n_head=2,
+    n_head_tile=2,
+    n_head_kv=2,
+)
 
 
 single_tile = LlamaAttentionTestParams(
@@ -211,3 +228,117 @@ class TestLlamaAttention:
             if p1.requires_grad:
                 g1, g2 = p1.grad, p2.grad
                 assert torch.norm(g1 - g2) <= rtol * torch.norm(g1)
+
+
+@pytest.mark.parametrize("bias", [True])
+@pytest.mark.parametrize(
+    "params",
+    [
+        pytest.param(single_tile, id="single_tile"),
+    ],
+)
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        "fp32",
+    ],
+)
+def test_llama_attn_forward_dynamic(
+    starpu_simple,
+    torch_rng,
+    dtype: str,
+    params: LlamaAttentionTestParams,
+    bias: bool,
+):
+    torch_layer, nntile_layer, x, pos_ids, mask, *_ = generate_inputs(
+        dtype, params, bias
+    )
+
+    y, _, _ = torch_layer(x, position_ids=pos_ids, attention_mask=mask)
+
+    input_x = x.cpu().numpy().T
+    y_nntile_logits = nntile_layer.forward_dynamic(
+        TensorMoments(nntc.from_array(input_x), None, False)
+    )
+    y_nntile = torch.Tensor(nntc.to_numpy(y_nntile_logits.value).T)
+
+    rtol = dtype2tol[dtype]["rtol"]
+    assert torch.norm(y - y_nntile) <= rtol * torch.norm(y)
+
+    truncate_to_size = 4
+    y_nntile_logits_trunc = nntile_layer.forward_dynamic(
+        TensorMoments(
+            nntc.from_array(input_x[:, :truncate_to_size, :]), None, False
+        )
+    )
+    y_nntile_trunc = torch.Tensor(nntc.to_numpy(y_nntile_logits_trunc.value).T)
+
+    y_torch_trunc, _, _ = torch_layer(
+        x[:, :truncate_to_size, :],
+        position_ids=pos_ids[:, :truncate_to_size],
+        attention_mask=mask[:, :, :truncate_to_size, :truncate_to_size],
+    )
+
+    rtol = dtype2tol[dtype]["rtol"]
+    assert torch.norm(y_torch_trunc - y_nntile_trunc) <= rtol * torch.norm(
+        y_torch_trunc
+    )
+
+    nntile_layer.unregister()
+    nntile_layer.x.unregister()
+    nntile_layer.y.unregister()
+
+
+@pytest.mark.parametrize("bias", [True])
+@pytest.mark.parametrize(
+    "params",
+    [
+        pytest.param(single_tile, id="single_tile"),
+        pytest.param(single_tile_trivial, id="single_tile_trivial"),
+    ],
+)
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        "fp32",
+    ],
+)
+def test_llama_attn_kvcache(
+    starpu_simple,
+    torch_rng,
+    dtype: str,
+    params: LlamaAttentionTestParams,
+    bias: bool,
+):
+    torch_layer, nntile_layer, x, pos_ids, mask, *_ = generate_inputs(
+        dtype, params, bias
+    )
+
+    prefill_size = 4
+    max_tokens = 8
+
+    inp_np = x.cpu().numpy().T
+    inp_prefill = nntc.from_array(inp_np[:, :prefill_size, 0:1])
+
+    outs_dyn = generate_greedy_logits_dynamic_kvcache(
+        nntile_layer, inp_prefill, prefill_size, max_tokens
+    )
+    outs_dyn_np = nntc.to_numpy(outs_dyn)
+
+    inp_prefill = nntc.from_array(inp_np[:, :prefill_size, 0:1])
+    outs_stat = generate_greedy_logits_padding(
+        nntile_layer, inp_prefill, prefill_size, max_tokens
+    )
+    outs_stat_np = nntc.to_numpy(outs_stat)
+
+    np.testing.assert_allclose(
+        outs_stat_np,
+        outs_dyn_np,
+        err_msg="test_kvcache: Dynamic does not match static",
+        rtol=dtype2tol[dtype]["rtol"],
+        atol=dtype2tol[dtype]["rtol"],
+    )
+
+    nntile_layer.unregister()
+    nntile_layer.x.unregister()
+    nntile_layer.y.unregister()
