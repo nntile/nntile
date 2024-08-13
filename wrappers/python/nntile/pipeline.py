0504026c8e14b0f5e598fec6e39de6bec75bba61
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2024-08-13 15:10:56 +0000

    Fix Xentropy test

--- wrappers/python/nntile/pipeline.py
+++ wrappers/python/nntile/pipeline.py
@@ -81,8 +81,7 @@ class Pipeline(object):
                     if p.grad_required:
                         p.grad.invalidate_submit()
                 # Limit parallelism through value of loss
-                loss_np = np.zeros((1,), dtype=np.float32, order="F")
-                self.loss.get_val(loss_np)
+                loss_np = self.loss.get_val()
                 self.loss_hist.append(loss_np[0])
                 # print("Loss in {} epoch = {}".format(i_epoch, loss_np[0]))
                 print("Batch={}/{} Epoch={}/{} Loss={}".format(
