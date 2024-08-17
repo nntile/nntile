d70a6e4ce9e391c7df2563ead7db83e981824d6e
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2024-08-17 21:11:43 +0300

    Fix tensor_prod.py tests

--- wrappers/python/tests/nntile_core/test_tensor_prod_inplace.py
+++ wrappers/python/tests/nntile_core/test_tensor_prod_inplace.py
@@ -6,8 +6,8 @@
 # NNTile is software framework for fast training of big neural networks on
 # distributed-memory heterogeneous systems based on StarPU runtime system.
 #
-# @file wrappers/python/tests/nntile_core/test_tensor_prod.py
-# Test for tensor::prod<T> Python wrapper
+# @file wrappers/python/tests/nntile_core/test_tensor_prod_inplace.py
+# Test for tensor::prod_inplace<T> Python wrapper
 #
 # @version 1.1.0
 
@@ -25,12 +25,12 @@ Tensor = {np.float32: nntile.tensor.Tensor_fp32,
           np.float64: nntile.tensor.Tensor_fp64}
 
 # Define mapping between tested function and numpy type
-prod = {np.float32: nntile.nntile_core.tensor.prod_fp32,
-        np.float64: nntile.nntile_core.tensor.prod_fp64}
+prod_inplace = {np.float32: nntile.nntile_core.tensor.prod_inplace_fp32,
+        np.float64: nntile.nntile_core.tensor.prod_inplace_fp64}
 
 
 @pytest.mark.parametrize('dtype', [np.float32, np.float64])
-def test_prod(dtype):
+def test_prod_inplace(dtype):
     # Describe single-tile tensor, located at node 0
     shape = [2, 3, 4]
     mpi_distr = [0]
@@ -40,8 +40,6 @@ def test_prod(dtype):
     A = Tensor[dtype](traits, mpi_distr, next_tag)
     next_tag = A.next_tag
     B = Tensor[dtype](traits, mpi_distr, next_tag)
-    next_tag = B.next_tag
-    C = Tensor[dtype](traits, mpi_distr, next_tag)
     # Set initial values of tensors
     rng = np.random.default_rng(42)
     rand_A = rng.standard_normal(shape)
@@ -50,12 +48,11 @@ def test_prod(dtype):
     rand_B = rng.standard_normal(shape)
     np_B = np.array(rand_B, dtype=dtype, order='F')
     B.from_array(np_B)
-    prod[dtype](A, B, C)
+    prod_inplace[dtype](A, B)
     np_C = np.zeros(shape, dtype=dtype, order='F')
-    C.to_array(np_C)
+    B.to_array(np_C)
     nntile.starpu.wait_for_all()
     A.unregister()
     B.unregister()
-    C.unregister()
     # Compare results
     assert_equal(np_C, np_A * np_B)
