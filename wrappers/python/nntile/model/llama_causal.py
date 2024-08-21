c4d7dfce69179f6fdcbecd1dd2c8e039d5af6452
svtdanny <sivtsovdt@gmail.com>
2024-08-21 14:34:16 +0300

    add dynamic llama

--- wrappers/python/nntile/model/llama_causal.py
+++ wrappers/python/nntile/model/llama_causal.py
@@ -16,7 +16,8 @@ from transformers import LlamaConfig as LlamaConfig_torch
 from transformers.models.llama.modeling_llama import (
     LlamaForCausalLM as LlamaCausalModel_torch)
 
-from nntile.wrappers.python.nntile.types import TensorMoments
+from nntile.model.generation.llm import LLMGenerationMixin
+from nntile.types import TensorMoments
 
 from ..layer import Linear
 from .base_model import BaseModel
@@ -24,7 +25,7 @@ from .llama import Llama as Llama_nntile
 from .llama_config import LlamaConfigNNTile
 
 
-class LlamaForCausalLM(BaseModel):
+class LlamaForCausalLM(BaseModel, LLMGenerationMixin):
     next_tag: int
     llama_model_: Llama_nntile
     lin_: Linear
@@ -36,6 +37,7 @@ class LlamaForCausalLM(BaseModel):
         self.dtype = config.dtype
 
         self.config = config
+        self.eos_token_id = self.config.eos_token_id
 
         if self.dtype not in ["fp32", "fp32_fast_tf32", "bf16"]:
             raise TypeError("Only fp32, fp32_fast_tf32 and bf16 are"
@@ -53,10 +55,44 @@ class LlamaForCausalLM(BaseModel):
 
         super().__init__(activations, layers)
 
-    def forward_dynamic(self, x: TensorMoments):
-        llama_logits = self.llama_model_.forward_dynamic(x)
+    def forward_dynamic(self, x: TensorMoments, use_cache: bool = False):
+        llama_logits = self.llama_model_.forward_dynamic(
+            x, use_cache=use_cache
+        )
         out_logits = self.lin_.forward_dynamic(llama_logits)
         return out_logits
+
+    @classmethod
+    def from_pretrained(
+        cls,
+        model_name: str,
+        seq_len: int,
+        batch_size: int = 1,
+        batch_size_tile: int | None = None,
+        seq_len_tile: int | None = None,
+        hidden_size_tile: int | None = None,
+        intermediate_size_tile: int | None = None,
+        n_head_tile: int | None = None,
+        dtype: str = 'fp32',
+        flash_attention: bool = False,
+        cache_dir: str | None = None,
+    ):
+        # TODO: where should be global repo with all this logic.
+        # We need to design it.
+        # For now, manual code for usability
+        return create_llama_model_from_torch_pretrained(
+            model_name=model_name,
+            batch_size=batch_size,
+            batch_size_tile=batch_size_tile,
+            seq_len=seq_len,
+            seq_len_tile=seq_len_tile,
+            hidden_size_tile=hidden_size_tile,
+            intermediate_size_tile=intermediate_size_tile,
+            n_head_tile=n_head_tile,
+            dtype=dtype,
+            flash_attention=flash_attention,
+            cache_dir=cache_dir,
+        )
 
     @staticmethod
     def from_torch(torch_llama_causal,
@@ -120,3 +156,60 @@ class LlamaForCausalLM(BaseModel):
         llama_model_torch.lm_head = self.lin_.to_torch_with_grads()
 
         return llama_model_torch
+
+
+def create_llama_model_from_torch_pretrained(
+    model_name: str,
+    batch_size: int,
+    batch_size_tile: int | None,
+    seq_len: int,
+    seq_len_tile: int | None,
+    hidden_size_tile: int | None,
+    intermediate_size_tile: int | None,
+    n_head_tile: int | None,
+    dtype: str,
+    flash_attention: bool,
+    cache_dir: str | None = None
+):
+    model_torch = LlamaCausalModel_torch.from_pretrained(
+        model_name, cache_dir=cache_dir
+    )
+    model_torch.eval()
+
+    llama_config_nntile = LlamaConfigNNTile(
+        vocab_size=model_torch.vocab_size,
+        vocab_embed_dim_tile=model_torch.config.hidden_size,
+        hidden_size=model_torch.config.hidden_size,
+        hidden_size_tile=hidden_size_tile or model_torch.config.hidden_size,
+        max_position_embeddings=model_torch.config.max_position_embeddings,
+        num_hidden_layers=model_torch.config.num_hidden_layers,
+        rms_norm_eps=model_torch.config.rms_norm_eps,
+        n_attention_head=model_torch.config.num_attention_heads,
+        num_key_value_heads=model_torch.config.num_key_value_heads,
+        intermediate_size=model_torch.config.intermediate_size,
+        intermediate_size_tile=intermediate_size_tile or model_torch.config.intermediate_size,  # noqa: E501
+        n_head_tile=n_head_tile or model_torch.config.num_attention_heads,
+        dtype=dtype,
+        flash_attention=flash_attention
+    )
+
+    single_batch_pos_ids = np.arange(seq_len).reshape(1, seq_len)
+    pos_ids = np.repeat(single_batch_pos_ids, batch_size, axis=0)
+
+    mask = np.array(np.triu(np.ones((seq_len, seq_len))),
+                        dtype=bool, order="F")
+
+    next_tag = 0
+    llama_causal_nntile, next_tag = LlamaForCausalLM.from_torch(
+        model_torch,
+        batch_size,
+        batch_size_tile or batch_size,
+        seq_len,
+        seq_len_tile or seq_len,
+        pos_ids,
+        mask,
+        llama_config_nntile,
+        next_tag
+    )
+
+    return llama_causal_nntile
