462cd79be21b30014a2b6cba38e31412a77096cc
svtdanny <sivtsovdt@gmail.com>
2024-09-03 13:56:29 +0300

    fix generation

--- wrappers/python/nntile/inference/llm_sync_engine.py
+++ wrappers/python/nntile/inference/llm_sync_engine.py
@@ -62,7 +62,7 @@ class LlmSyncInferenceEngine:
         output_ids_np = output_ids_np[:effective_size]
 
         # construct generation result
-        generated_text = self.tokenizer.decode(output_ids_np)
+        generated_text = self.tokenizer.decode(output_ids_np.flatten())
         return generated_text
 
 
