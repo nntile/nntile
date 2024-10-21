61a58977f9a5187b8ef0466855df865ea2e79a24
gogoglrind <gogolgrind@gmail.com>
2024-10-21 14:29:26 +0000

    quick fix

--- wrappers/python/nntile/layer/batch_norm.py
+++ wrappers/python/nntile/layer/batch_norm.py
@@ -16,8 +16,8 @@ import math
 from nntile.layer.base_layer import BaseLayer
 from nntile.tensor import (
     TensorMoments, add_fiber_async, add_fiber_inplace_async, add_inplace_async,
-    copy_async, empty, hypot_scalar_inverse_async, norm_fiber_async, ones,
-    pow_async, prod_async, prod_fiber3_async, prod_fiber_async,
+    copy_async, empty, hypot_scalar_inverse_async, norm_fiber_inplace_async,
+    ones, pow_async, prod_async, prod_fiber3_async, prod_fiber_async,
     prod_inplace_async, sum_fiber_async, sumprod_fiber_async)
 
 
