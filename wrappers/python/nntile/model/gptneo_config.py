71c9faaebe76853849f51d4696786e1f69f6a916
Gleb Karpov <gleborg.mipt@gmail.com>
2024-11-27 13:14:14 +0300

    GPTNeo MLP refactoring (#202)
    * Start of GPTNeo refactoring.
    Added GPTNeoMLP module with test.
    
    * Linting fix
    
    * Linting fix

---
+++ wrappers/python/nntile/model/gptneo_config.py
@@ -0,0 +1,36 @@
+# @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+#                              (Skoltech), Russia. All rights reserved.
+#                2023-present Artificial Intelligence Research Institute
+#                              (AIRI), Russia. All rights reserved.
+#
+# NNTile is software framework for fast training of big neural networks on
+# distributed-memory heterogeneous systems based on StarPU runtime system.
+#
+# @file wrappers/python/nntile/model/gptneo_config.py
+# GPT Neo model config
+#
+# @version 1.1.0
+
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTNeoConfig:
+    vocab_size: int
+    vocab_embed_dim_tile: int
+    hidden_size: int
+    hidden_size_tile: int
+    intermediate_size: int
+    intermediate_size_tile: int
+    num_heads: int
+    num_heads_tile: int
+    activation_function: str = "gelutanh"
+    dtype: str = "fp32"
+    flash_attention: bool = False
+    layer_norm_epsilon: float = 1e-5
+    max_position_embeddings: int = 2048
+    num_hidden_layers: int = 1
+    redux: bool = False
+    bos_token_id: int = 50256
+    eos_token_id: int = 50256
+    name: str = "gpt-neo"
---
+++ wrappers/python/nntile/model/gptneo_mlp.py
@@ -0,0 +1,110 @@
+# @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+#                              (Skoltech), Russia. All rights reserved.
+#                2023-present Artificial Intelligence Research Institute
+#                              (AIRI), Russia. All rights reserved.
+#
+# NNTile is software framework for fast training of big neural networks on
+# distributed-memory heterogeneous systems based on StarPU runtime system.
+#
+# @file wrappers/python/nntile/model/gptneo_mlp.py
+# GPTNeoMLP submodule of NNTile Python package
+#
+# @version 1.1.0
+
+import torch
+from transformers.models.gpt_neo.modeling_gpt_neo import (
+    GPTNeoConfig as GPTNeoConfigTorch, GPTNeoMLP as GPTNeoMlpTorch)
+
+from nntile.tensor import TensorMoments, notrans, to_numpy
+
+from ..layer.act import Act
+from ..layer.linear import Linear
+from .base_model import BaseModel
+from .gptneo_config import GPTNeoConfig
+
+
+class GPTNeoMLP(BaseModel):
+    next_tag: int
+
+    # Construct model with all the provided data
+    def __init__(self, x: TensorMoments,
+                 config: GPTNeoConfig, next_tag: int):
+        # Init activations and list of layers
+        activations = [x]
+        layers = []
+        self.hidden_size = config.hidden_size
+        hidden_size_tile = config.hidden_size_tile
+        self.intermediate_size = config.intermediate_size
+        intermediate_size_tile = config.intermediate_size_tile
+        activation_function = config.activation_function
+        redux = config.redux
+        gemm_ndim = 1
+        # Initial linear layer that converts input to internal shape
+        new_layer, next_tag = Linear.generate_simple(
+            x,
+            "R",
+            notrans,
+            gemm_ndim,
+            [self.intermediate_size],
+            [intermediate_size_tile],
+            next_tag,
+            redux=redux,
+        )
+        layers.append(new_layer)
+        activations.extend(new_layer.activations_output)
+
+        new_layer, next_tag = Act.generate_simple(
+            activations[-1], activation_function, next_tag
+        )
+        layers.append(new_layer)
+        activations.extend(new_layer.activations_output)
+
+        new_layer, next_tag = Linear.generate_simple(
+            activations[-1],
+            "R",
+            notrans,
+            gemm_ndim,
+            [self.hidden_size],
+            [hidden_size_tile],
+            next_tag,
+            redux=redux,
+        )
+        layers.append(new_layer)
+        activations.extend(new_layer.activations_output)
+        self.next_tag = next_tag
+        # Fill Base Model with the generated data
+        super().__init__(activations, layers, config)
+
+    @staticmethod
+    def from_torch(
+        mlp_torch, x: TensorMoments, config: GPTNeoConfig, next_tag: int
+    ):
+        """
+        torch_mlp is PyTorch MLP where no biases in linear layers
+        """
+        mlp_nntile = GPTNeoMLP(x, config, next_tag)
+        torch_params = list(mlp_torch.parameters())
+        for i, p in enumerate(mlp_nntile.parameters):
+            p.value.from_array(torch_params[i].cpu().detach().numpy())
+        return mlp_nntile, mlp_nntile.next_tag
+
+    def to_torch(self):
+        config_torch = GPTNeoConfigTorch()
+        config_torch.hidden_size = self.hidden_size
+        mlp_torch = GPTNeoMlpTorch(self.intermediate_size, config_torch)
+        for p_nntile, p_torch in zip(self.parameters,
+                                     mlp_torch.parameters()):
+            p_torch.data = torch.tensor(to_numpy(p_nntile.value),
+                                        requires_grad=True)
+        return mlp_torch
+
+    def to_torch_with_grads(self):
+        config_torch = GPTNeoConfigTorch()
+        config_torch.hidden_size = self.hidden_size
+        mlp_torch = GPTNeoMlpTorch(self.intermediate_size, config_torch)
+        for p_nntile, p_torch in zip(self.parameters,
+                                     mlp_torch.parameters()):
+            p_torch.data = torch.tensor(to_numpy(p_nntile.value),
+                                        requires_grad=True)
+            p_torch.grad = torch.tensor(to_numpy(p_nntile.grad))
+        return mlp_torch
