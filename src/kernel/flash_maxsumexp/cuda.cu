b69b1cc23171147807e2c99d079a32e1c7acc174
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2025-02-24 10:42:32 +0300

    FA works for TF32 version of flash_maxsumexp

--- src/kernel/flash_maxsumexp/cuda.cu
+++ src/kernel/flash_maxsumexp/cuda.cu
@@ -17,30 +17,91 @@
 #include <algorithm>
 #include <cmath>
 #include <limits>
-#include <cutlass/cutlass.h>
-#include <cutlass/gemm/device/gemm.h>
-#include <cutlass/matrix_coord.h>
-#include <cutlass/tensor_ref.h>
-#include <cutlass/layout/matrix.h>
+#include <mma.h>  // Include WMMA header
 
 namespace nntile::kernel::flash_maxsumexp
 {
 
-template<typename T>
-__global__ void flash_maxsumexp_kernel(Index batch, Index seq, Index head,
+template<typename T, typename Y, Index SEQ_BLOCK_SIZE, Index HEAD_SIZE>
+static __device__ void gemm_smem_sync(const T smem_K[SEQ_BLOCK_SIZE][HEAD_SIZE],
+        const T smem_Q[HEAD_SIZE][SEQ_BLOCK_SIZE],
+        const bool_t smem_mask[SEQ_BLOCK_SIZE][SEQ_BLOCK_SIZE],
+        T scale,
+        Y smem_QK[SEQ_BLOCK_SIZE][SEQ_BLOCK_SIZE])
+{
+    using namespace nvcuda::wmma;
+    // Special case for nntile::fp32_fast_tf32_t using WMMA
+    if constexpr (std::is_same<T, nntile::fp32_fast_tf32_t>::value)
+    {
+        auto smem_K_float = reinterpret_cast<const float *>(smem_K);
+        auto smem_Q_float = reinterpret_cast<const float *>(smem_Q);
+        // Define fragments using float precision TF32 supports only 16x16x8 grid
+        fragment<matrix_a, 16, 16, 8, precision::tf32, row_major> a_frag;
+        fragment<matrix_b, 16, 16, 8, precision::tf32, row_major> b_frag;
+        fragment<accumulator, 16, 16, 8, float> c_frag;
+
+        // Load data into fragments and perform matrix multiplication
+        for(int i = 0; i < SEQ_BLOCK_SIZE; i += 16)
+        {
+            for(int j = 0; j < SEQ_BLOCK_SIZE; j += 16)
+            {
+                // Initialize accumulator fragment
+                fill_fragment(c_frag, 0.0f);
+                for(int k = 0; k < HEAD_SIZE; k += 8)
+                {
+                    // Load matrix A and B from shared memory into fragments
+                    // K[SEQ_BLOCK_SIZE, HEAD_SIZE] - leading dimension is HEAD_SIZE
+                    load_matrix_sync(a_frag,
+                        smem_K_float + i * HEAD_SIZE + k, HEAD_SIZE);
+                    // Q[HEAD_SIZE, SEQ_BLOCK_SIZE] - leading dimension is SEQ_BLOCK_SIZE
+                    load_matrix_sync(b_frag,
+                        smem_Q_float + k * SEQ_BLOCK_SIZE + j, SEQ_BLOCK_SIZE);
+                    // Perform matrix multiplication and accumulate
+                    mma_sync(c_frag, a_frag, b_frag, c_frag);
+                }
+                // Apply scale factor to the result
+                for(int k = 0; k < c_frag.num_elements; ++k)
+                {
+                    c_frag.x[k] *= float{scale};
+                }
+                __syncthreads();
+                // Store results back to shared memory
+                store_matrix_sync(&smem_QK[i][j], c_frag, SEQ_BLOCK_SIZE, mem_row_major);
+            }
+        }
+    }
+    // General case
+    else
+    {
+        for(int i = threadIdx.x; i < SEQ_BLOCK_SIZE; i += blockDim.x)
+        {
+            for(int j = threadIdx.y; j < SEQ_BLOCK_SIZE; j += blockDim.y)
+            {
+                Y sum = 0;
+                for(int k = 0; k < HEAD_SIZE; ++k)
+                {
+                    // K[i,k] * Q[k,j] for K @ Q
+                    sum += Y(smem_K[i][k]) * Y(smem_Q[k][j]);
+                }
+                smem_QK[i][j] = sum * Y(scale);
+            }
+        }
+        __syncthreads();
+    }
+}
+
+template<typename T, Index SEQ_BLOCK_SIZE, Index HEAD_SIZE>
+__global__ void flash_maxsumexp_kernel(Index batch, Index seq,
         T scale, const T *K, const T *Q, const bool_t *mask, T *maxsumexp)
 {
     using Scalar = typename T::repr_t;
+    // Accumulator is fp64_t for fp64_t input type and fp32_t for other input types
     using ElementAccumulator = typename std::conditional<
-        std::is_same<T, fp64_t>::value, double, float>::type;
-
-    // Define tile sizes
-    constexpr int SEQ_BLOCK_SIZE = 32; // Sequence is processed in parallel
-    constexpr int HEAD_SIZE = 64; // Each head is processed in a single block
+        std::is_same<T, nntile::fp64_t>::value, double, float>::type;
 
     // Shared memory
     __shared__ T smem_Q[HEAD_SIZE][SEQ_BLOCK_SIZE];
-    __shared__ T smem_K[HEAD_SIZE][SEQ_BLOCK_SIZE];
+    __shared__ T smem_K[SEQ_BLOCK_SIZE][HEAD_SIZE];
     __shared__ bool_t smem_mask[SEQ_BLOCK_SIZE][SEQ_BLOCK_SIZE];
     __shared__ ElementAccumulator smem_max[SEQ_BLOCK_SIZE];
     __shared__ ElementAccumulator smem_sumexp[SEQ_BLOCK_SIZE];
@@ -65,14 +126,14 @@ __global__ void flash_maxsumexp_kernel(Index batch, Index seq, Index head,
     }
     __syncthreads();
 
-    // Load Q[0:head, block_row:block_row+SEQ_BLOCK_SIZE, batch_idx]
-    for(int i = threadIdx.x; i < head; i += blockDim.x)
+    // Load Q[0:HEAD_SIZE, block_row:block_row+SEQ_BLOCK_SIZE, batch_idx]
+    for(int i = threadIdx.x; i < HEAD_SIZE; i += blockDim.x)
     {
         for(int j = threadIdx.y; j < SEQ_BLOCK_SIZE; j += blockDim.y)
         {
             if(block_col + j < seq)
             {
-                smem_Q[i][j] = Q[i + head * (block_col + j) + head * seq * batch_idx];
+                smem_Q[i][j] = Q[i + HEAD_SIZE * (block_col + j) + HEAD_SIZE * seq * batch_idx];
             }
         }
     }
@@ -89,14 +150,14 @@ __global__ void flash_maxsumexp_kernel(Index batch, Index seq, Index head,
             }
         }
 
-        // Load K[0:head, block_col:block_col+SEQ_BLOCK_SIZE, batch_idx]
-        for(int i = threadIdx.x; i < head; i += blockDim.x)
+        // Load K[0:HEAD_SIZE, block_col:block_col+SEQ_BLOCK_SIZE, batch_idx]
+        for(int i = threadIdx.x; i < HEAD_SIZE; i += blockDim.x)
         {
             for(int j = threadIdx.y; j < SEQ_BLOCK_SIZE; j += blockDim.y)
             {
                 if(block_row + j < seq)
                 {
-                    smem_K[i][j] = K[i + head * (block_row + j) + head * seq * batch_idx];
+                    smem_K[j][i] = K[i + HEAD_SIZE * (block_row + j) + HEAD_SIZE * seq * batch_idx];
                 }
             }
         }
@@ -115,26 +176,16 @@ __global__ void flash_maxsumexp_kernel(Index batch, Index seq, Index head,
         __syncthreads();
 
         // Compute tile product scale * K^T @ Q in Fortran order
-        for(int i = threadIdx.x; i < SEQ_BLOCK_SIZE; i += blockDim.x)
-        {
-            if(block_row + i < seq)  // Row of K^T
-            {
-                for(int j = threadIdx.y; j < SEQ_BLOCK_SIZE; j += blockDim.y)
-                {
-                    if(block_col + j < seq)  // Column of Q
-                    {
-                        ElementAccumulator sum = 0;
-                        for(int k = 0; k < head; ++k)
-                        {
-                            // K[k,i] * Q[k,j] for K^T @ Q
-                            sum += Scalar(smem_K[k][i]) * Scalar(smem_Q[k][j]);
-                        }
-                        smem_QK[i][j] = sum * ElementAccumulator(scale);
-                    }
-                }
-            }
-        }
-        __syncthreads();
+        gemm_smem_sync<T, ElementAccumulator, SEQ_BLOCK_SIZE, HEAD_SIZE>(smem_K,
+                smem_Q, smem_mask, scale, smem_QK);
+        // __syncthreads();
+        // for(int i = threadIdx.x; i < SEQ_BLOCK_SIZE; i += blockDim.x)
+        // {
+        //     for(int j = threadIdx.y; j < SEQ_BLOCK_SIZE; j += blockDim.y)
+        //     {
+        //         smem_QK[i][j] *= ElementAccumulator(scale);
+        //     }
+        // }
 
         // Compute max in each row
         for(int j = threadIdx.x; j < SEQ_BLOCK_SIZE; j += blockDim.x)
@@ -198,16 +249,33 @@ void cuda(cudaStream_t stream, Index batch, Index seq, Index head,
           const T *K, const T *Q, const bool_t *mask, T *maxsumexp) noexcept
 {
     // Calculate grid size for tiles
-    dim3 blocks((seq + 31)/32, batch);
-    dim3 threads(32, 32);  // 1024 threads per block
+    dim3 blocks((seq + 15)/16, batch);
+    dim3 threads(16, 16);  // 256 threads per block
 
     // Calculate scale factor 1/sqrt(head)
     using Y = typename T::repr_t;
     T scale = T{Y(1) / std::sqrt(Y(head))};
 
     // Launch kernel
-    flash_maxsumexp_kernel<T><<<blocks, threads, 0, stream>>>(
-        batch, seq, head, scale, K, Q, mask, maxsumexp);
+    if(head == 64)
+    {
+        flash_maxsumexp_kernel<T, 16, 64><<<blocks, threads, 0, stream>>>(
+            batch, seq, scale, K, Q, mask, maxsumexp);
+    }
+    else if(head == 128)
+    {
+        flash_maxsumexp_kernel<T, 16, 128><<<blocks, threads, 0, stream>>>(
+            batch, seq, scale, K, Q, mask, maxsumexp);
+    }
+    else if(head == 256)
+    {
+        flash_maxsumexp_kernel<T, 16, 256><<<blocks, threads, 0, stream>>>(
+            batch, seq, scale, K, Q, mask, maxsumexp);
+    }
+    else
+    {
+        std::cerr << "Unsupported head size" << std::endl;
+    }
 }
 
 // Explicit instantiation
