1f5bcdf6c2c59085d9a020c560eafe072053f3dd
pavel.tikhomirov <pavel.tikhomirov@skoltech.ru>
2025-03-12 12:00:01 +0300

    remove coments

--- include/nntile/kernel/lars_step/cuda.hh
+++ include/nntile/kernel/lars_step/cuda.hh
@@ -19,8 +19,7 @@
 
 namespace nntile::kernel::lars_step
 {
-    // Index num_iter, Index num_elems, Index num_steps, Scalar gamma_0, Scalar momentum,
-    // Scalar weight_decay, Scalar lars_coefficient, const T *grad, T *momentum_buffer, T *p
+
 template<typename T>
 void cuda(cudaStream_t stream, Index num_iter, Index num_elems, Index num_steps,
     Scalar gamma_0, Scalar momentum, Scalar weight_decay, Scalar lars_coefficient,
