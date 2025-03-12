4d3699344678842d9ffec4118cd1a384caa86c3d
Yaroslav Abramov <117538994+yarick-abramov@users.noreply.github.com>
2025-03-12 11:46:58 +0300

    Create cpu.cc
    LARS optimizer Step on cpu

---
+++ src/kernel/lars_step/cpu.cc
@@ -0,0 +1,43 @@
+#include "nntile/tile/lars_step.hh"
+#include "nntile/starpu/lars_step.hh"
+#include <cmath>
+#include <vector>
+
+namespace nntile::kernel::lars_step {
+
+template<typename T>
+void lars_step(Index num_iter, Index num_steps, Index num_elems, Scalar gamma_0, Scalar momentum, Scalar weight_decay, Scalar lars_coefficient,
+                           const T *grad, T *momentum_buffer, T *weights) noexcept
+{
+    using Y = typename T::repr_t;
+    const Y beta = weight_decay;
+    
+    // Compute the learning rate adjustment
+    Y gamma_t = gamma_0 * std::pow(1 - static_cast<Y>(num_iter) / static_cast<Y>(num_steps), 2);
+        
+    // Cycle over the parameters (num_elems)
+    for(Index i = 0; i < num_elems; ++i)
+        {
+            // Obtain the stochastic gradient for the current mini-batch
+            Y grad_val = static_cast<Y>(grad[i]);
+           
+            // Compute local learning rate
+            Y local_lr = std::sqrt(std::norm(static_cast<Y>(weights[i]))) / 
+                         (std::norm(grad_val) + beta * std::norm(static_cast<Y>(weights[i])));
+            
+            // Update the momentum
+            momentum_buffer[i] = momentum * static_cast<Y>(momentum_buffer[i]) + 
+                                 gamma_t * local_lr * (grad_val + beta * static_cast<Y>(weights[i]));
+            
+            // Update the weights
+            weights[i] -= momentum_buffer[i];
+        }
+    
+}
+
+// Explicit instantiation
+template
+void lars_step<fp32_t>(Index num_iter, Index num_steps, Index num_elems, Scalar gamma_0, Scalar momentum, Scalar weight_decay, Scalar lars_coefficient,
+                                    const fp32_t *grad, fp32_t *momentum_buffer, fp32_t *weights) noexcept;
+
+} // namespace nntile::kernel::lars_step
