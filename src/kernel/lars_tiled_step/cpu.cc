6d4dd542bb84673e51aa399ce8a2e6e2e37f98de
Yaroslav Abramov <117538994+yarick-abramov@users.noreply.github.com>
2025-03-13 13:44:12 +0300

    Update cpu.cc

--- src/kernel/lars_tiled_step/cpu.cc
+++ src/kernel/lars_tiled_step/cpu.cc
@@ -1,38 +1,42 @@
-#include "nntile/tile/lars_tiled_step.hh"
-#include "nntile/starpu/lars_tiled_step.hh"
+#include "nntile/tile/lars_step.hh"
+#include "nntile/starpu/lars_step.hh"
 #include <cmath>
 #include <vector>
+#include <cblas.h> // Include CBLAS header for using cblas_dnrm2
 
-namespace nntile::kernel::lars_tiled_step {
+namespace nntile::kernel::lars_step {
 
 template<typename T>
-void lars_tiled_step(Index num_iter, Index num_elems, Index num_steps, Scalar gamma_0, Scalar momentum, Scalar weight_decay, Scalar lars_coefficient,
-                           const T *grad, T *momentum_buffer, T *weights) noexcept
+void lars_step(Index num_iter, Index num_elems, Index num_steps, Scalar gamma_0, Scalar momentum, Scalar weight_decay, Scalar lars_coefficient,
+               const T *grad, T *momentum_buffer, T *weights) noexcept
 {
     using Y = typename T::repr_t;
     const Y beta = weight_decay;
     
     // Compute the learning rate adjustment
     Y gamma_t = gamma_0 * std::pow(1 - static_cast<Y>(num_iter) / static_cast<Y>(num_steps), 2);
+
+    Y norm_weights = cblas_dnrm2(num_elems, &weights, 1);  // L2 norm of weights
+    Y norm_grad = cblas_dnrm2(num_elems, &grad_val, 1);       // L2 norm of grad
+
+    // Compute local learning rate
+    Y local_lr = std::sqrt(norm_weights) / 
+                     (norm_grad + beta * norm_weights);
         
     // Cycle over the parameters (num_elems)
     for(Index i = 0; i < num_elems; ++i)
-        {
-            // Obtain the stochastic gradient for the current mini-batch
-            Y grad_val = static_cast<Y>(grad[i]);
-           
-            // Compute local learning rate
-            Y local_lr = std::sqrt(std::norm(static_cast<Y>(weights[i]))) / 
-                         (std::norm(grad_val) + beta * std::norm(static_cast<Y>(weights[i])));
-            
-            // Update the momentum
-            momentum_buffer[i] = momentum * static_cast<Y>(momentum_buffer[i]) + 
-                                 gamma_t * local_lr * (grad_val + beta * static_cast<Y>(weights[i]));
-            
-            // Update the weights
-            weights[i] = weights[i] - momentum_buffer[i];
-        }
-    
+    {
+        // Obtain the stochastic gradient for the current mini-batch
+        Y p_val=static_cast<Y>(weights[i]), grad_val = static_cast<Y>(grad[i]);
+        
+        // Update the momentum
+        Y momentum_buffer_val = momentum * static_cast<Y>(momentum_buffer[i]) + 
+                             gamma_t * local_lr * (grad_val + beta * static_cast<Y>(weights[i]));
+        momentum_buffer[i] = static_cast<T>(momentum_buffer_val);
+
+        // Update the weights
+        weights[i] = static_cast<T>(p_val - momentum_buffer_val);
+    }
 }
 
 // Explicit instantiation
