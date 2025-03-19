4ee9ddc44325cee9ea2a758c13216df51a642a94
pqviet2208 <87597259+pqviet2208@users.noreply.github.com>
2025-03-19 22:24:13 +0300

    Create cuda.hh

---
+++ include/nntile/kernel/lion_step/cuda.hh
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <nntile/base_types.hh>
+#include <cuda_runtime.h>
+
+namespace nntile::kernel::lion_step
+{
+
+template<typename T>
+void cuda(cudaStream_t stream,
+          Index num_iter,
+          Index num_elems,
+          Scalar beta_1,
+          Scalar beta_2,
+          Scalar eps,
+          Scalar lr,
+          Scalar weight_decay,
+          const T *grad,
+          T *first_moment,
+          T *p)
+    noexcept;
+
+} // namespace nntile::kernel::lion_step
