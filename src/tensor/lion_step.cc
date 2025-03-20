fa7e85c87aa20fabdad17cc23ff951a80cea5d92
pqviet2208 <87597259+pqviet2208@users.noreply.github.com>
2025-03-20 18:14:04 +0300

    Update lion_step.cc
    Delete eps and second_moment

--- src/tensor/lion_step.cc
+++ src/tensor/lion_step.cc
@@ -20,8 +20,8 @@
  
  //! Asynchronous tensor-wise fuse Lion step
  template<typename T>
- void lion_step_async(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-                     const Tensor<T> &grad, const Tensor<T> &first_moment, const Tensor<T> &second_moment,
+ void lion_step_async(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+                     const Tensor<T> &grad, const Tensor<T> &first_moment,
                      const Tensor<T> &p)
  {
      if (p.matrix_shape != grad.matrix_shape)
@@ -34,10 +34,10 @@
          throw std::runtime_error("Parameter shape is not equal to first_moment shape");
      }
  
-     if (p.matrix_shape != second_moment.matrix_shape)
-     {
-         throw std::runtime_error("Parameter shape is not equal to second_moment shape");
-     }
+     // if (p.matrix_shape != second_moment.matrix_shape)
+     // {
+     //     throw std::runtime_error("Parameter shape is not equal to second_moment shape");
+     // }
  
      int mpi_size = starpu_mpi_world_size();
      int mpi_rank = starpu_mpi_world_rank();
@@ -53,7 +53,7 @@
          int p_tile_rank = p_tile_handle.mpi_get_rank();
          int grad_tile_rank = grad_tile_handle.mpi_get_rank();
          int first_moment_tile_rank = first_moment_tile_handle.mpi_get_rank();
-         int second_moment_tile_rank = second_moment_tile_handle.mpi_get_rank();
+         // int second_moment_tile_rank = second_moment_tile_handle.mpi_get_rank();
          // Transfer data
          grad_tile_handle.mpi_transfer(p_tile_rank, mpi_rank);
          first_moment_tile_handle.mpi_transfer(p_tile_rank, mpi_rank);
@@ -62,9 +62,10 @@
          if(mpi_rank == p_tile_rank)
          {
              auto traits = p.get_tile_traits(i);
-             starpu::lion_step::submit<T>(num_iter, traits.nelems, beta_1, beta_2, eps, lr, weight_decay,
+             starpu::lion_step::submit<T>(num_iter, traits.nelems, beta_1, beta_2, lr, weight_decay,
                                           grad_tile_handle, first_moment_tile_handle,
-                                          second_moment_tile_handle, p_tile_handle);
+                                          // second_moment_tile_handle,
+                                          p_tile_handle);
          }
          // Flush cache for the output tile on every node
          p_tile_handle.mpi_flush();
@@ -73,76 +74,77 @@
  
  //! Blocking version of tensor-wise Lion operation
  template<typename T>
- void lion_step(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-                const Tensor<T> &grad, const Tensor<T> &first_moment, const Tensor<T> &second_moment,
+ void lion_step(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+                const Tensor<T> &grad, const Tensor<T> &first_moment, 
+                // const Tensor<T> &second_moment,
                 const Tensor<T> &p)
  {
-     lion_step_async<T>(num_iter, beta_1, beta_2, eps, lr, weight_decay, grad, first_moment, second_moment, p);
+     lion_step_async<T>(num_iter, beta_1, beta_2, lr, weight_decay, grad, first_moment, p);
      starpu_task_wait_for_all();
      starpu_mpi_wait_for_all(MPI_COMM_WORLD);
  }
  
  // Explicit instantiation
  template
- void lion_step_async<fp32_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-     const Tensor<fp32_t> &grad, const Tensor<fp32_t> &first_moment, const Tensor<fp32_t> &second_moment,
+ void lion_step_async<fp32_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+     const Tensor<fp32_t> &grad, const Tensor<fp32_t> &first_moment,
                     const Tensor<fp32_t> &p);
  
  template
- void lion_step_async<fp32_fast_tf32_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-     const Tensor<fp32_fast_tf32_t> &grad, const Tensor<fp32_fast_tf32_t> &first_moment, const Tensor<fp32_fast_tf32_t> &second_moment,
+ void lion_step_async<fp32_fast_tf32_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+     const Tensor<fp32_fast_tf32_t> &grad, const Tensor<fp32_fast_tf32_t> &first_moment,
                     const Tensor<fp32_fast_tf32_t> &p);
  
  template
- void lion_step_async<fp32_fast_fp16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-                                   const Tensor<fp32_fast_fp16_t> &grad, const Tensor<fp32_fast_fp16_t> &first_moment, const Tensor<fp32_fast_fp16_t> &second_moment,
+ void lion_step_async<fp32_fast_fp16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+                                   const Tensor<fp32_fast_fp16_t> &grad, const Tensor<fp32_fast_fp16_t> &first_moment, 
                                    const Tensor<fp32_fast_fp16_t> &p);
  
  template
- void lion_step_async<fp32_fast_bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-                                   const Tensor<fp32_fast_bf16_t> &grad, const Tensor<fp32_fast_bf16_t> &first_moment, const Tensor<fp32_fast_bf16_t> &second_moment,
+ void lion_step_async<fp32_fast_bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+                                   const Tensor<fp32_fast_bf16_t> &grad, const Tensor<fp32_fast_bf16_t> &first_moment, 
                                    const Tensor<fp32_fast_bf16_t> &p);
  
  template
- void lion_step_async<fp64_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-     const Tensor<fp64_t> &grad, const Tensor<fp64_t> &first_moment, const Tensor<fp64_t> &second_moment,
+ void lion_step_async<fp64_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+     const Tensor<fp64_t> &grad, const Tensor<fp64_t> &first_moment, 
                     const Tensor<fp64_t> &p);
  
  template
- void lion_step_async<bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-     const Tensor<bf16_t> &grad, const Tensor<bf16_t> &first_moment, const Tensor<bf16_t> &second_moment,
+ void lion_step_async<bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+     const Tensor<bf16_t> &grad, const Tensor<bf16_t> &first_moment, 
                     const Tensor<bf16_t> &p);
  
  // Explicit instantiation
  template
- void lion_step<fp32_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-     const Tensor<fp32_t> &grad, const Tensor<fp32_t> &first_moment, const Tensor<fp32_t> &second_moment,
+ void lion_step<fp32_t>(Index num_iter, Scalar beta_1, Scalar beta_2,  Scalar lr, Scalar weight_decay,
+     const Tensor<fp32_t> &grad, const Tensor<fp32_t> &first_moment, 
                     const Tensor<fp32_t> &p);
  
  template
- void lion_step<fp32_fast_tf32_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-                                   const Tensor<fp32_fast_tf32_t> &grad, const Tensor<fp32_fast_tf32_t> &first_moment, const Tensor<fp32_fast_tf32_t> &second_moment,
+ void lion_step<fp32_fast_tf32_t>(Index num_iter, Scalar beta_1, Scalar beta_2,  Scalar lr, Scalar weight_decay,
+                                   const Tensor<fp32_fast_tf32_t> &grad, const Tensor<fp32_fast_tf32_t> &first_moment, 
                                    const Tensor<fp32_fast_tf32_t> &p);
  
  template
- void lion_step<fp32_fast_fp16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-                                   const Tensor<fp32_fast_fp16_t> &grad, const Tensor<fp32_fast_fp16_t> &first_moment, const Tensor<fp32_fast_fp16_t> &second_moment,
+ void lion_step<fp32_fast_fp16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+                                   const Tensor<fp32_fast_fp16_t> &grad, const Tensor<fp32_fast_fp16_t> &first_moment, 
                                    const Tensor<fp32_fast_fp16_t> &p);
  
  template
- void lion_step<fp32_fast_bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-                                   const Tensor<fp32_fast_bf16_t> &grad, const Tensor<fp32_fast_bf16_t> &first_moment, const Tensor<fp32_fast_bf16_t> &second_moment,
+ void lion_step<fp32_fast_bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+                                   const Tensor<fp32_fast_bf16_t> &grad, const Tensor<fp32_fast_bf16_t> &first_moment, 
                                    const Tensor<fp32_fast_bf16_t> &p);
  
  
  template
- void lion_step<fp64_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-     const Tensor<fp64_t> &grad, const Tensor<fp64_t> &first_moment, const Tensor<fp64_t> &second_moment,
+ void lion_step<fp64_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+     const Tensor<fp64_t> &grad, const Tensor<fp64_t> &first_moment, 
                     const Tensor<fp64_t> &p);
  
  template
- void lion_step<bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar eps, Scalar lr, Scalar weight_decay,
-     const Tensor<bf16_t> &grad, const Tensor<bf16_t> &first_moment, const Tensor<bf16_t> &second_moment,
+ void lion_step<bf16_t>(Index num_iter, Scalar beta_1, Scalar beta_2, Scalar lr, Scalar weight_decay,
+     const Tensor<bf16_t> &grad, const Tensor<bf16_t> &first_moment,
                     const Tensor<bf16_t> &p);
  
  } // namespace nntile::tensor
