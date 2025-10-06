3d0f9298cdcf65464594f96b17ed355d800aada5
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2025-10-06 18:19:13 +0300

    Update norm_fiber test

--- tests/kernel/norm_fiber.cc
+++ tests/kernel/norm_fiber.cc
@@ -51,8 +51,12 @@ struct TestData
     Index m, n, k, batch; // Dimensions
     Scalar alpha, beta;    // Scaling factors
 
-    std::vector<T> src1;
-    std::vector<T> src2;
+    Y eps_check;
+
+    std::vector<T> src1_init;
+    std::vector<T> src2_init;
+    std::vector<T> dst_init;
+
     std::vector<T> dst_ref;
 };
 
@@ -79,15 +83,16 @@ void reference_norm_fiber(TestData<T>& data)
             {
                 for(Index i0 = 0; i0 < data.m; ++i0)
                 {
-                    Index src_idx = ((i1 + b * data.n) * data.k + i2) * data.m + i0;
-                    ref_t val = static_cast<Y>(data.src1[src_idx]);
+                    Index src_idx = (i1 + b * data.n) * data.k + i2;
+                    src_idx = src_idx * data.m + i0;
+                    ref_t val = static_cast<Y>(data.src1_init[src_idx]);
                     norm_sq += val * val;
                 }
             }
             ref_t norm_val = std::sqrt(norm_sq) * alpha_r;
-            ref_t src2_val = beta_r * static_cast<Y>(data.src2[i2 + b * data.k]);
-            ref_t result = std::hypot(norm_val, src2_val);
-            data.dst_ref[i2 + b * data.k] = static_cast<T>(static_cast<Y>(result));
+            ref_t src2_val = static_cast<Y>(data.src2_init[i2 + b * data.k]);
+            ref_t result = std::hypot(norm_val, beta_r * src2_val);
+            data.dst_ref[i2 + b * data.k] = static_cast<Y>(result);
         }
     }
 }
@@ -106,8 +111,9 @@ void generate_data(TestData<T>& data, DataGen strategy)
     using Y = typename T::repr_t;
     const Index total_elems = data.m * data.n * data.k * data.batch;
 
-    data.src1.resize(total_elems);
-    data.src2.resize(data.k * data.batch);
+    data.src1_init.resize(total_elems);
+    data.src2_init.resize(data.k * data.batch);
+    data.dst_init.resize(data.k * data.batch);
     data.dst_ref.resize(data.k * data.batch);
 
     switch(strategy)
@@ -118,13 +124,19 @@ void generate_data(TestData<T>& data, DataGen strategy)
             {
                 for(Index i2 = 0; i2 < data.k; ++i2)
                 {
-                    data.src2[i2 + b * data.k] = Y{2.0 * i2 + 1.0};
+                    Y src2_val = static_cast<Y>(2.0 * i2 + 1.0);
+                    data.src2_init[i2 + b * data.k] = src2_val;
+                    Y dst_val = static_cast<Y>(-1.0);
+                    data.dst_init[i2 + b * data.k] = dst_val;
                     for(Index i1 = 0; i1 < data.n; ++i1)
                     {
                         for(Index i0 = 0; i0 < data.m; ++i0)
                         {
-                            Index src_idx = ((i1 + b * data.n) * data.k + i2) * data.m + i0;
-                            data.src1[src_idx] = Y{1.0 + i0 + i1 * data.m + i2 * data.m * data.n};
+                            Index src_idx = (i1 + b * data.n) * data.k + i2;
+                            src_idx = src_idx * data.m + i0;
+                            Y src1_val = static_cast<Y>(1.0 + i0 + i1 * data.m +
+                                i2 * data.m * data.n);
+                            data.src1_init[src_idx] = src1_val;
                         }
                     }
                 }
@@ -136,11 +148,12 @@ void generate_data(TestData<T>& data, DataGen strategy)
             std::uniform_real_distribution<Y> dist(-2.0, 2.0);
             for(Index i = 0; i < total_elems; ++i)
             {
-                data.src1[i] = dist(gen);
+                data.src1_init[i] = dist(gen);
             }
             for(Index i = 0; i < data.k * data.batch; ++i)
             {
-                data.src2[i] = dist(gen);
+                data.src2_init[i] = dist(gen);
+                data.dst_init[i] = dist(gen);
             }
             break;
     }
@@ -169,6 +182,28 @@ TestData<T> get_test_data(
     // Generate data by a provided strategy
     generate_data(data, strategy);
 
+    // Set accuracy threshold for each precision
+    if (std::is_same_v<T, bf16_t>)
+    {
+        data.eps_check = 1e-1;
+    }
+    else if (std::is_same_v<T, fp16_t>)
+    {
+        data.eps_check = 1e-2;
+    }
+    else if (std::is_same_v<T, fp32_t>)
+    {
+        data.eps_check = 3.1e-3;
+    }
+    else if (std::is_same_v<T, fp64_t>)
+    {
+        data.eps_check = 1e-7;
+    }
+    else
+    {
+        throw std::runtime_error("Unsupported data type");
+    }
+
     // Compute reference outputs
     reference_norm_fiber(data);
     return data;
@@ -178,38 +213,31 @@ TestData<T> get_test_data(
 template<typename T>
 void verify_results(
     const TestData<T>& data,
-    const std::vector<T>& dst_out
+    const std::vector<T>& src1,
+    const std::vector<T>& src2,
+    const std::vector<T>& dst
 )
 {
     using Y = typename T::repr_t;
-    // Set accuracy threshold for each precision
-    Y eps_check;
-    if (std::is_same_v<T, bf16_t>)
+
+    // Check that src1 and src2 were not changed during kernel execution
+    for(Index i = 0; i < data.src1_init.size(); ++i)
     {
-        eps_check = 1e-1;
+        REQUIRE(static_cast<Y>(src1[i]) == static_cast<Y>(data.src1_init[i]));
     }
-    else if (std::is_same_v<T, fp16_t>)
+    for(Index i = 0; i < data.src2_init.size(); ++i)
     {
-        eps_check = 1e-2;
-    }
-    else if (std::is_same_v<T, fp32_t>)
-    {
-        eps_check = 3.1e-3;
-    }
-    else if (std::is_same_v<T, fp64_t>)
-    {
-        eps_check = 1e-7;
-    }
-    else
-    {
-        throw std::runtime_error("Unsupported data type");
+        REQUIRE(static_cast<Y>(src2[i]) == static_cast<Y>(data.src2_init[i]));
     }
 
+    // Check that dst (output) matches reference
     for(Index i = 0; i < data.dst_ref.size(); ++i)
     {
         Y ref = static_cast<Y>(data.dst_ref[i]);
-        Y val = static_cast<Y>(dst_out[i]);
-        REQUIRE_THAT(val, WithinRel(ref, eps_check) || WithinAbs(ref, eps_check));
+        REQUIRE_THAT(
+            static_cast<Y>(dst[i]),
+            WithinRel(ref, data.eps_check)
+        );
     }
 }
 
@@ -217,7 +245,9 @@ void verify_results(
 template<typename T, bool run_bench>
 void run_cpu_test(TestData<T>& data)
 {
-    std::vector<T> dst_cpu(data.k * data.batch);
+    std::vector<T> dst_cpu(data.dst_init);
+    std::vector<T> src1_cpu(data.src1_init);
+    std::vector<T> src2_cpu(data.src2_init);
 
     if constexpr (run_bench)
     {
@@ -236,9 +266,9 @@ void run_cpu_test(TestData<T>& data)
                 data.k,
                 data.batch,
                 data.alpha,
-                &data.src1[0],
+                &src1_cpu[0],
                 data.beta,
-                &data.src2[0],
+                &src2_cpu[0],
                 &dst_cpu[0]
             );
         };
@@ -251,12 +281,12 @@ void run_cpu_test(TestData<T>& data)
             data.k,
             data.batch,
             data.alpha,
-            &data.src1[0],
+            &src1_cpu[0],
             data.beta,
-            &data.src2[0],
+            &src2_cpu[0],
             &dst_cpu[0]
         );
-        verify_results(data, dst_cpu);
+        verify_results(data, src1_cpu, src2_cpu, dst_cpu);
     }
 }
 
@@ -267,19 +297,50 @@ template<typename T, bool run_bench>
 void run_cuda_test(TestData<T>& data)
 {
     T *dev_src1, *dev_src2, *dev_dst;
-    CUDA_CHECK(cudaMalloc(&dev_src1, sizeof(T) * data.src1.size()),
-               "cudaMalloc dev_src1");
-    CUDA_CHECK(cudaMalloc(&dev_src2, sizeof(T) * data.src2.size()),
-               "cudaMalloc dev_src2");
-    CUDA_CHECK(cudaMalloc(&dev_dst, sizeof(T) * data.k * data.batch),
-               "cudaMalloc dev_dst");
+    CUDA_CHECK(
+        cudaMalloc(&dev_src1, sizeof(T) * data.src1_init.size()),
+        "cudaMalloc dev_src1"
+    );
+    CUDA_CHECK(
+        cudaMalloc(&dev_src2, sizeof(T) * data.src2_init.size()),
+        "cudaMalloc dev_src2"
+    );
+    CUDA_CHECK(
+        cudaMalloc(&dev_dst, sizeof(T) * data.dst_init.size()),
+        "cudaMalloc dev_dst"
+    );
 
-    std::vector<T> dst_cuda(data.k * data.batch);
+    std::vector<T> dst_cuda(data.dst_init);
+    std::vector<T> src1_cuda(data.src1_init);
+    std::vector<T> src2_cuda(data.src2_init);
 
-    CUDA_CHECK(cudaMemcpy(dev_src1, &data.src1[0], sizeof(T) * data.src1.size(),
-                          cudaMemcpyHostToDevice), "cudaMemcpy dev_src1");
-    CUDA_CHECK(cudaMemcpy(dev_src2, &data.src2[0], sizeof(T) * data.src2.size(),
-                          cudaMemcpyHostToDevice), "cudaMemcpy dev_src2");
+    CUDA_CHECK(
+        cudaMemcpy(
+            dev_src1,
+            &src1_cuda[0],
+            sizeof(T) * data.src1_init.size(),
+            cudaMemcpyHostToDevice
+        ),
+        "cudaMemcpy dev_src1"
+    );
+    CUDA_CHECK(
+        cudaMemcpy(
+            dev_src2,
+            &src2_cuda[0],
+            sizeof(T) * data.src2_init.size(),
+            cudaMemcpyHostToDevice
+        ),
+        "cudaMemcpy dev_src2"
+    );
+    CUDA_CHECK(
+        cudaMemcpy(
+            dev_dst,
+            &dst_cuda[0],
+            sizeof(T) * data.dst_init.size(),
+            cudaMemcpyHostToDevice
+        ),
+        "cudaMemcpy dev_dst"
+    );
 
     cudaStream_t stream;
     CUDA_CHECK(cudaStreamCreate(&stream), "cudaStreamCreate");
@@ -326,10 +387,35 @@ void run_cuda_test(TestData<T>& data)
         );
         CUDA_CHECK(cudaStreamSynchronize(stream), "cudaStreamSynchronize");
 
-        CUDA_CHECK(cudaMemcpy(&dst_cuda[0], dev_dst, sizeof(T) * data.k * data.batch,
-                              cudaMemcpyDeviceToHost), "cudaMemcpy dst_cuda");
+        CUDA_CHECK(
+            cudaMemcpy(
+                &dst_cuda[0],
+                dev_dst,
+                sizeof(T) * data.dst_init.size(),
+                cudaMemcpyDeviceToHost
+            ),
+            "cudaMemcpy dst_cuda"
+        );
+        CUDA_CHECK(
+            cudaMemcpy(
+                &src1_cuda[0],
+                dev_src1,
+                sizeof(T) * data.src1_init.size(),
+                cudaMemcpyDeviceToHost
+            ),
+            "cudaMemcpy src1_cuda"
+        );
+        CUDA_CHECK(
+            cudaMemcpy(
+                &src2_cuda[0],
+                dev_src2,
+                sizeof(T) * data.src2_init.size(),
+                cudaMemcpyDeviceToHost
+            ),
+            "cudaMemcpy src2_cuda"
+        );
 
-        verify_results(data, dst_cuda);
+        verify_results(data, src1_cuda, src2_cuda, dst_cuda);
     }
 
     CUDA_CHECK(cudaFree(dev_src1), "cudaFree dev_src1");
