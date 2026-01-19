a3bc2f557432df671dfb1482c6b70789d80c129c
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2026-01-19 16:38:25 +0300

    Fix MacOS compilation

--- include/nntile/kernel/cblas.hh
+++ include/nntile/kernel/cblas.hh
@@ -27,15 +27,15 @@
 #   else // Use standard int otherwise
 #       define CBLAS_INT int
 #   endif
-#elif __has_include(<vecLib/cblas.h>) // Look for Apple Accelerate framework
-#   include <vecLib/cblas.h>
+#elif defined(__APPLE__) && __has_include(<Accelerate/Accelerate.h>) // Look for Apple Accelerate framework
+#   include <Accelerate/Accelerate.h>
 #   define CBLAS_INT int
 #elif __has_include(<mkl.h>) // Look for MKL (OneAPI) header
 #   include <mkl.h>
 #   define CBLAS_INT MKL_INT
 #else
-#   error "CBLAS header not found (tried <cblas.h>, <vecLib/cblas.h>, and <mkl.h>)"
-#endif // __has_include(<cblas.h>) || __has_include(<vecLib/cblas.h>) || __has_include(<mkl.h>)
+#   error "CBLAS header not found (tried <cblas.h>, <Accelerate/Accelerate.h>, and <mkl.h>)"
+#endif // __has_include(<cblas.h>) || __has_include(<Accelerate/Accelerate.h>) || __has_include(<mkl.h>)
 
 // Other NNTile headers
 #include <nntile/base_types.hh>
