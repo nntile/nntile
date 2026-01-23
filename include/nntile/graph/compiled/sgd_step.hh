d8e982077cbfb2032b7f50feca7dd1cbef73b8bd
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2026-01-23 15:55:10 +0300

    Fix legal headers

--- include/nntile/graph/compiled/sgd_step.hh
+++ include/nntile/graph/compiled/sgd_step.hh
@@ -1,3 +1,17 @@
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
+ *
+ * NNTile is software framework for fast training of big neural networks on
+ * distributed-memory heterogeneous systems based on StarPU runtime system.
+ *
+ * @file include/nntile/graph/compiled/sgd_step.hh
+ * Compiled graph sgd_step operation.
+ *
+ * @version 1.1.0
+ * */
+
 #pragma once
 
 #include <nntile/graph/compiled_graph.hh>
--- tests/graph/compiled/hypot.cc
+++ tests/graph/compiled/hypot.cc
@@ -1,3 +1,17 @@
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
+ *
+ * NNTile is software framework for fast training of big neural networks on
+ * distributed-memory heterogeneous systems based on StarPU runtime system.
+ *
+ * @file tests/graph/compiled/hypot.cc
+ * Test for compiled graph hypot operation.
+ *
+ * @version 1.1.0
+ * */
+
 #include "compiled_test_utils.hh"
 
 #include "nntile/tensor/hypot.hh"
