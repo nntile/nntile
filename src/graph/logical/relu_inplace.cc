2dba0673c14309dc932fbe31d2b21959746f6aa6
Aleksandr Mikhalev <al.mikhalev@skoltech.ru>
2026-01-25 17:05:18 +0300

    Checkpoint

---
+++ src/graph/logical/relu_inplace.cc
@@ -0,0 +1,40 @@
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
+ *
+ * NNTile is software framework for fast training of big neural networks on
+ * distributed-memory heterogeneous systems based on StarPU runtime system.
+ *
+ * @file src/graph/logical/relu_inplace.cc
+ * Logical graph ReLU in-place operation.
+ *
+ * @version 1.1.0
+ * */
+
+// Include corresponding header
+#include "nntile/graph/logical/relu_inplace.hh"
+
+// Include standard headers
+#include <stdexcept>
+#include <utility>
+
+// Include other NNTile headers
+#include <nntile/graph/logical_graph.hh>
+
+namespace nntile::graph
+{
+
+//! ReLU in-place: x = relu(x)
+void relu_inplace(LogicalGraph::TensorNode& x)
+{
+    OpAttrs attrs = GeluAttrs{};
+    x.graph().add_op(
+        OpType::RELU_INPLACE,
+        attrs,
+        {&x},
+        {&x}
+    );
+}
+
+} // namespace nntile::graph
