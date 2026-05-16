9e63d11b032e407fd77636080620e012f4f58f17
Aleksandr Katrutsa <amkatrutsa@gmail.com>
2026-05-16 23:56:48 +0300

    Add tile functions

---
+++ src/tile/isfinite.cc
@@ -0,0 +1,93 @@
+/*! @copyright (c) 2022-present Skolkovo Institute of Science and Technology
+ *                              (Skoltech), Russia. All rights reserved.
+ *                 2023-present Artificial Intelligence Research Institute
+ *                              (AIRI), Russia. All rights reserved.
+ *
+ * NNTile is software framework for fast training of big neural networks on
+ * distributed-memory heterogeneous systems based on StarPU runtime system.
+ *
+ * @file src/tile/isfinite.cc
+ * Check NaN or Inf elements for Tile<T>
+ *
+ * @version 1.1.0
+ * */
+
+#include "nntile/tile/isfinite.hh"
+#include "nntile/starpu/isfinite.hh"
+#include "nntile/starpu/config.hh"
+
+namespace nntile::tile
+{
+
+//! Asynchronous tile-wise check NaN or Inf values
+/*! @param[inout] A: Tile for the element-wise check of NaN or Inf
+ *  @param[inout] flag: indicator of NaN or Inf values
+ * */
+template<typename T>
+void isfinite_async(const Tile<T> &A, const Tile<Index> &flag)
+{
+    int mpi_rank = starpu_mpi_world_rank();
+    int a_rank = A.mpi_get_rank();
+    if(mpi_rank == a_rank)
+    {
+        // Submit task without any arguments checked
+        starpu::isfinite.submit<std::tuple<T>>(A.nelems, A, flag);
+    }
+}
+
+//! Blocking version of tile-wise check NaN or Inf values
+/*! @param[inout] A: Tile for the check NaN or Inf values
+ *  @param[inout] flag: indicator of NaN or Inf values
+ * */
+template<typename T>
+void isfinite(const Tile<T> &A, const Tile<Index> &flag)
+{
+    isfinite_async<T>(A, flag);
+    starpu_task_wait_for_all();
+}
+
+// Explicit instantiation
+template
+void isfinite_async<fp32_t>(const Tile<fp32_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite_async<fp64_t>(const Tile<fp64_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite_async<bf16_t>(const Tile<bf16_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite_async<fp32_fast_tf32_t>(
+        const Tile<fp32_fast_tf32_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite_async<fp32_fast_fp16_t>(
+        const Tile<fp32_fast_fp16_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite_async<fp32_fast_bf16_t>(
+        const Tile<fp32_fast_bf16_t> &A, const Tile<Index> &flag);
+
+// Explicit instantiation
+template
+void isfinite<fp32_t>(const Tile<fp32_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite<fp64_t>(const Tile<fp64_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite<bf16_t>(const Tile<bf16_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite<fp32_fast_tf32_t>(
+        const Tile<fp32_fast_tf32_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite<fp32_fast_fp16_t>(
+        const Tile<fp32_fast_fp16_t> &A, const Tile<Index> &flag);
+
+template
+void isfinite<fp32_fast_bf16_t>(
+        const Tile<fp32_fast_bf16_t> &A, const Tile<Index> &flag);
+
+} // namespace nntile::tile
